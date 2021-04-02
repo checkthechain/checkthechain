6b974b9542223ec9c02e1f5394b4a915464ea855
sslivkoff <stormslivkoff@gmail.com>
2021-04-02 00:19:35 -0700

    overhaul data conversions to arrays, dataframes, csv, and json

---
+++ tests/export/test_data_export.py
@@ -0,0 +1,51 @@
+import tempfile
+
+import fei
+from fei.toolbox import json_utils
+
+
+n_blocks = 1000
+
+parameters = {
+    'run_duration': n_blocks,
+    'record_events': True,
+}
+
+simulation = fei.FeiSimulation(**parameters)
+simulation.run()
+
+
+def test_snapshots_to_csv():
+
+    # write file
+    handle, tmp_path = tempfile.mkstemp()
+    simulation.snapshots_to_csv(path=tmp_path, overwrite=True)
+
+    # assert snapshots match
+    with open(tmp_path, 'r') as f:
+        lines = f.readlines()
+    assert len(lines) == len(simulation.snapshots) + 1
+
+
+def test_events_to_json():
+
+    # write file
+    handle, tmp_path = tempfile.mkstemp()
+    simulation.events_to_json(path=tmp_path, overwrite=True)
+
+    # assert event records match
+    with open(tmp_path, 'r') as f:
+        event_data = json_utils.load_with_decimals(f)
+
+    pairs = [
+        [event_data['event_records'], simulation.event_records],
+        [event_data['event_pre_metadatas'], simulation.event_pre_metadatas],
+        [event_data['event_post_metadatas'], simulation.event_post_metadatas],
+        [event_data['event_pre_metadata_fields'], simulation.event_pre_metadata_fields],
+        [event_data['event_post_metadata_fields'], simulation.event_post_metadata_fields],
+    ]
+    for lhs, rhs in pairs:
+        lhs_serialized = json_utils.dumps_with_decimals(lhs, sort_keys=True)
+        rhs_serialized = json_utils.dumps_with_decimals(rhs, sort_keys=True)
+        assert lhs_serialized == rhs_serialized
+
