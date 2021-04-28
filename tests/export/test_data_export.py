d2c91b7563b188af2b58c1bbfe101f05ee6563ea
sslivkoff <stormslivkoff@gmail.com>
2021-04-28 12:34:09 -0700

    rename and repackge decimal_utils

--- tests/export/test_data_export.py
+++ tests/export/test_data_export.py
@@ -1,6 +1,6 @@
 import tempfile
 
-import ten
+from larp.toolbox import decimal_utils
 
 import fei
 
@@ -36,17 +36,23 @@ def test_events_to_json():
 
     # assert event records match
     with open(tmp_path, 'r') as f:
-        event_data = ten.json_load(f)
+        event_data = decimal_utils.json_load(f)
 
     pairs = [
         [event_data['event_records'], simulation.event_records],
         [event_data['event_pre_metadatas'], simulation.event_pre_metadatas],
         [event_data['event_post_metadatas'], simulation.event_post_metadatas],
-        [event_data['event_pre_metadata_fields'], simulation.event_pre_metadata_fields],
-        [event_data['event_post_metadata_fields'], simulation.event_post_metadata_fields],
+        [
+            event_data['event_pre_metadata_fields'],
+            simulation.event_pre_metadata_fields,
+        ],
+        [
+            event_data['event_post_metadata_fields'],
+            simulation.event_post_metadata_fields,
+        ],
     ]
     for lhs, rhs in pairs:
-        lhs_serialized = ten.json_dumps(lhs, sort_keys=True)
-        rhs_serialized = ten.json_dumps(rhs, sort_keys=True)
+        lhs_serialized = decimal_utils.json_dumps(lhs, sort_keys=True)
+        rhs_serialized = decimal_utils.json_dumps(rhs, sort_keys=True)
         assert lhs_serialized == rhs_serialized
 
