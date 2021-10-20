85698c52b1e051b550f0dee1ba8508299eb3029e
sslivkoff <stormslivkoff@gmail.com>
2021-10-20 11:44:25 -0700

    add invalid data checks to etherscan contract_abi scraper

--- src/ctc/evm/contract_abi_utils/contract_abi_backends/etherscan_contract_abis.py
+++ src/ctc/evm/contract_abi_utils/contract_abi_backends/etherscan_contract_abis.py
@@ -2,11 +2,17 @@ import json
 
 import requests
 
+from ... import address_utils
+
 
 def get_contract_abi_from_etherscan(contract_address):
     """fetch contract abi using etherscan"""
+    if not address_utils.is_address_str(contract_address):
+        raise Exception('not a valid address: ' + str(contract_address))
     url_template = 'http://api.etherscan.io/api?module=contract&action=getabi&address={address}&format=raw'
     abi_endpoint = url_template.format(address=contract_address)
     abi = json.loads(requests.get(abi_endpoint).text)
+    if isinstance(abi, dict) and abi.get('status') == '0':
+        raise Exception('could not obtain contract abi from etherscan for ' + str(contract_address))
     return abi
 
