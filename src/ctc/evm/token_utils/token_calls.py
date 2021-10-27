6aedb47de6253f0174a4bfa132dde2442830286a
sslivkoff <stormslivkoff@gmail.com>
2021-10-26 18:13:46 -0700

    convert token_utils to use contract_addresses instead of contract_names in all cases

--- src/ctc/evm/token_utils/token_calls.py
+++ src/ctc/evm/token_utils/token_calls.py
@@ -1,4 +1,6 @@
+from ctc import directory
 from ctc.toolbox import web3_utils
+from .. import address_utils
 from .. import contract_abi_utils
 from .. import token_utils
 
@@ -24,15 +26,24 @@ def call_token_contract(
     )
 
 
-def _get_web3_token_contract(token=None, abi=None, **contract_kwargs):
+def _get_web3_token_contract(
+    token=None, contract_address=None, contract_abi=None, **contract_kwargs
+):
 
-    if abi is None:
-        abi = 'ERC20'
+    if token is not None and contract_address is None:
+        if not address_utils.is_address_str(token):
+            contract_address = directory.token_addresses[token.lower()]
+        else:
+            contract_address = token
+
+    if contract_abi is None:
+        contract_abi = contract_abi_utils.get_contract_abi(
+            contract_address=contract_address
+        )
 
     return web3_utils.get_web3_contract(
         contract_address=token_utils.get_token_address(token),
-        contract_abi=contract_abi_utils.load_named_contract_abi(contract_name=abi),
+        contract_abi=contract_abi,
         **contract_kwargs
     )
-
 
