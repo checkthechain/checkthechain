257524a78ad36bfd5c6c416ff727c1f726ddc920
sslivkoff <stormslivkoff@gmail.com>
2021-10-26 18:09:36 -0700

    remove web3py dependence for checksums

--- src/ctc/evm/address_utils/address_data.py
+++ src/ctc/evm/address_utils/address_data.py
@@ -1,4 +1,5 @@
-import web3
+# import web3
+from .. import binary_utils
 
 
 def is_address_str(some_str):
@@ -39,7 +40,37 @@ def create_hash_preview(
 
 
 def get_address_checksum(address):
-    return web3.Web3.toChecksumAddress(address)
+    """
+
+    - adapted from eth_utils.to_checksum_address()
+    """
+    # return web3.Web3.toChecksumAddress(address)
+
+    # validate address
+    address_format = binary_utils.get_binary_format(address)
+    if address_format not in ['prefix_hex', 'raw_hex']:
+        raise Exception('checksum only relevant to hex formatted addresses')
+    address = binary_utils.convert_binary_format(address, 'raw_hex')
+
+    # compute address hash
+    address_hash = binary_utils.keccak_text(address, output_format='raw_hex')
+
+    # assemble checksum
+    chars = []
+    for address_char, hash_char in zip(address, address_hash):
+        if int(hash_char, 16) > 7:
+            chars.append(address_char.upper())
+        else:
+            chars.append(address_char)
+    raw_checksum = ''.join(chars)
+
+    # convert to output format
+    if address_format == 'raw_hex':
+        return raw_checksum
+    elif address_format == 'prefix_hex':
+        return binary_utils.convert_binary_format(raw_checksum, 'prefix_hex')
+    else:
+        raise Exception('checksum only relevant to hex formatted addresses')
 
 
 def create_reverse_address_map(
@@ -55,7 +86,7 @@ def create_reverse_address_map(
         if include_lower:
             reverse_map[address.lower()] = name
         if include_checksum:
-            reverse_map[web3.Web3.toChecksumAddress(address)] = name
+            reverse_map[get_address_checksum(address)] = name
 
     return reverse_map
 
