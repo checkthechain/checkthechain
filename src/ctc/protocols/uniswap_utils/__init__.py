7ec8b8eeb373be34b0774c2a454c90299e08b6ab
sslivkoff <stormslivkoff@gmail.com>
2021-10-28 01:04:58 -0700

    clean version of uniswap_utils

--- src/ctc/protocols/uniswap_utils/__init__.py
+++ src/ctc/protocols/uniswap_utils/__init__.py
@@ -2,3 +2,5 @@ from .uniswap_ops import *
 from .uniswap_queries import *
 from .uniswap_etl import *
 
+from .uniswap_v2_crud import *
+
---
+++ src/ctc/protocols/uniswap_utils/uniswap_v2_crud.py
@@ -0,0 +1,88 @@
+from ctc import evm
+
+
+#
+# # metadata
+#
+
+
+def get_pool_tokens(pool_address):
+    token0 = evm.eth_call(contract_address=pool_address, function_name='token0')
+    token1 = evm.eth_call(contract_address=pool_address, function_name='token1')
+    return [token0, token1]
+
+
+def get_pool_metadata(pool_address):
+    token_x, token_y = get_pool_tokens(pool_address=pool_address)
+    token_x_name = evm.fetch_token_name(token=token_x)
+    token_y_name = evm.fetch_token_name(token=token_y)
+    return {
+        'x_name': token_x_name,
+        'y_name': token_y_name,
+        'x_address': token_x,
+        'y_address': token_y,
+    }
+
+
+#
+# # state
+#
+
+
+@evm.parallelize_block_fetching()
+def get_v2_pool_state(pool_address, block='latest'):
+
+    block = evm.normalize_block(block=block)
+
+    token_x, token_y = get_pool_tokens(pool_address=pool_address)
+    token_x_reserves = evm.fetch_token_balance_of(
+        pool_address, token=token_x, block=block
+    )
+    token_y_reserves = evm.fetch_token_balance_of(
+        pool_address, token=token_y, block=block
+    )
+    lp_total_supply = evm.fetch_token_total_supply(
+        token=pool_address, block=block
+    )
+
+    return {
+        'x_reserves': token_x_reserves,
+        'y_reserves': token_y_reserves,
+        'lp_total_supply': lp_total_supply,
+    }
+
+
+#
+# # events
+#
+
+
+def get_v2_pool_swaps(pool_address, start_block=None, end_block=None):
+
+    return evm.get_events(
+        event_name='Swap',
+        contract_address=pool_address,
+        start_block=start_block,
+        end_block=end_block,
+    )
+
+
+def get_v2_pool_mints(pool_address, start_block=None, end_block=None):
+
+    return evm.get_events(
+        event_name='Mint',
+        contract_address=pool_address,
+        start_block=start_block,
+        end_block=end_block,
+    )
+
+
+def get_v2_pool_burns(pool_address, start_block=None, end_block=None):
+
+    return evm.get_events(
+        event_name='Burn',
+        contract_address=pool_address,
+        start_block=start_block,
+        end_block=end_block,
+    )
+
