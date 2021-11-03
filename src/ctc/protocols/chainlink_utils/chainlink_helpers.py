6efbd1e841cfc6a3815f9c708a952da0341d4e68
sslivkoff <stormslivkoff@gmail.com>
2021-11-03 14:52:05 -0700

    convert chainlink feed creation retrieval to new evm functions

--- src/ctc/protocols/chainlink_utils/chainlink_helpers.py
+++ src/ctc/protocols/chainlink_utils/chainlink_helpers.py
@@ -1,7 +1,4 @@
-import eth_abi
-import numpy as np
-import web3
-
+from ctc import directory
 from ctc import evm
 from . import chainlink_fetch
 
@@ -14,70 +11,14 @@ def fetch_eth_price(**kwargs):
 
 
 def find_feed_first_block(
-    feed, start_search=None, end_search=None, verbose=True, condition='nonzero'
+    feed, start_search=None, end_search=None, verbose=True
 ):
-    if start_search is None:
-        start_search = 0
-    if end_search is None:
-        end_search = evm.fetch_latest_block_number()
-
-    kwargs = {
-        'feed': feed,
-        'results': {},
-        'verbose': verbose,
-        'condition': condition,
-    }
-
-    if _is_feed_started(block=start_search, **kwargs):
-        raise Exception('feed might start before start_search')
-    if not _is_feed_started(block=end_search, **kwargs):
-        raise Exception('feed might end after end_search')
-
-    while True:
-        midpoint = (start_search + end_search) / 2
-        midpoint = int(midpoint)
-
-        if _is_feed_started(block=midpoint, **kwargs):
-            end_search = midpoint
-        else:
-            start_search = midpoint
-
-        if start_search + 1 == end_search:
-            return end_search
-        elif _is_feed_started(block=start_search, **kwargs):
-            return start_search
-        elif not _is_feed_started(block=end_search, **kwargs):
-            return end_search + 1
-
-
-def _is_feed_started(feed, block, results, verbose, condition):
-
-    if block not in results:
-
-        try:
-            datum = chainlink_fetch.fetch_feed_datum(feed, block=block)
-
-            if condition == 'exists':
-                results[block] = True
-            elif condition == 'nonzero':
-                results[block] = not np.isclose(datum['answer'], 0)
-            else:
-                raise Exception('unknown condition: ' + str(condition))
-
-        except (
-            eth_abi.exceptions.InsufficientDataBytes,
-            web3.exceptions.BadFunctionCallOutput,
-            ValueError,
-        ):
-            results[block] = False
-
-        if verbose:
-            print(
-                'queried block='
-                + str(block)
-                + ', active='
-                + str(results[block])
-            )
-
-    return results[block]
+    if feed in directory.chainlink_feeds:
+        feed = directory.chainlink_feeds[feed]
+    return evm.get_contract_creation_block(
+        contract_address=feed,
+        start_block=start_search,
+        end_block=end_search,
+        verbose=verbose,
+    )
 
