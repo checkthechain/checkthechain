e3169ba3f1275c7852b3579bb8ba4af348fa922f
sslivkoff <stormslivkoff@gmail.com>
2021-11-08 00:10:50 -0800

    use an http session for each process

--- src/ctc/evm/rpc_utils/rpc_backends/rpc_http.py
+++ src/ctc/evm/rpc_utils/rpc_backends/rpc_http.py
@@ -1,6 +1,9 @@
 import json
+import os
 
-import requests
+
+
+_http_sessions = {}
 
 
 def rpc_call_http(method, parameters, provider=None, decode_result=True):
@@ -13,10 +16,36 @@ def rpc_call_http(method, parameters, provider=None, decode_result=True):
         'id': 1,
     }
 
+    # get session
+    session = _get_http_session(provider=provider)
+
     # perform request
-    response = requests.post(url=provider, data=json.dumps(data))
+    response = session.post(url=provider, data=json.dumps(data))
     response_data = response.json()
     result = response_data['result']
 
     return result
+
+
+def _get_http_session(provider):
+
+    # ensure sessions are not shared across processes
+    pid = os.getpid()
+    session_id = (pid, provider)
+
+    if provider not in _http_sessions:
+        import requests
+        from requests.adapters import HTTPAdapter
+        from requests.packages.urllib3.util.retry import Retry
+
+        session = requests.Session()
+
+        retry = Retry(connect=10, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+
+        _http_sessions[session_id] = session
+
+    return _http_sessions[session_id]
 
