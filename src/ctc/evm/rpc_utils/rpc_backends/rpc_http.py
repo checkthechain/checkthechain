be856fed1c307b36f2be5eefc4e3a484b6788d1b
sslivkoff <stormslivkoff@gmail.com>
2021-11-09 17:14:43 -0800

    allow batched rpc calls in rpc_call_http

--- src/ctc/evm/rpc_utils/rpc_backends/rpc_http.py
+++ src/ctc/evm/rpc_utils/rpc_backends/rpc_http.py
@@ -1,30 +1,80 @@
 import json
 import os
 
+from ctc import config_utils
 
 
 _http_sessions = {}
 
 
-def rpc_call_http(method, parameters, provider=None, decode_result=True):
+def rpc_call_http(
+    method=None,
+    parameters=None,
+    batch_methods=None,
+    batch_parameters=None,
+    provider=None,
+):
 
-    # assemble payload
-    data = {
-        'jsonrpc': '2.0',
-        'method': method,
-        'params': parameters,
-        'id': 1,
-    }
+    if provider is None:
+        provider = config_utils.get_config()['export_provider']
 
     # get session
     session = _get_http_session(provider=provider)
 
+    # build request data
+    data = _build_request_data(
+        method=method,
+        parameters=parameters,
+        batch_methods=batch_methods,
+        batch_parameters=batch_parameters,
+    )
+
     # perform request
     response = session.post(url=provider, data=json.dumps(data))
     response_data = response.json()
-    result = response_data['result']
 
-    return result
+    if batch_parameters is not None:
+        results = sorted(response_data, key=lambda r: r['id'])
+        return [result['result'] for result in results]
+    else:
+        return response_data['result']
+
+
+def _build_request_data(
+    method=None, parameters=None, batch_methods=None, batch_parameters=None
+):
+
+    # assemble payload
+    if parameters is not None:
+        return {
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': parameters,
+            'id': 1,
+        }
+
+    elif batch_parameters is not None:
+
+        if method is None and batch_methods is None:
+            raise Exception('must specify method or batch_methods')
+        if batch_methods is None:
+            batch_methods = [method for b in range(len(batch_parameters))]
+        ids = range(1, len(batch_parameters) + 1)
+
+        data = []
+        for id, method, parameters in zip(ids, batch_methods, batch_parameters):
+            datum = {
+                'jsonrpc': '2.0',
+                'method': method,
+                'params': parameters,
+                'id': id,
+            }
+            data.append(datum)
+
+        return data
+
+    else:
+        raise Exception('must specify parameters or batch_parameters')
 
 
 def _get_http_session(provider):
