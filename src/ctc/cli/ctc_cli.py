4f01f28cfc3b23775717b6dbe98a516affb69846
sslivkoff <stormslivkoff@gmail.com>
2021-11-12 13:58:42 -0800

    hardcode command_index into ctc cli

--- src/ctc/cli/ctc_cli.py
+++ src/ctc/cli/ctc_cli.py
@@ -3,9 +3,32 @@ import toolcli
 
 def create_ctc_cli():
 
-    command_indices = toolcli.autoindex_command_root(
-        root_module_name='ctc.cli.commands',
-    )
+    # hardcode this for speed
+    # command_indices = {}
+    # roots = [
+    #     'ctc.cli.commands',
+    #     'ctc.protocols.fei_utils.cli',
+    # ]
+    # for root in roots:
+    #     branch = toolcli.autoindex_command_root(root_module_name=root)
+    #     overlap = set(branch.keys()).intersection(command_indices.keys())
+    #     if len(overlap) > 0:
+    #         raise Exception('command_index key collision: ' + str(overlap))
+    #     command_indices.update(branch)
+    # print('command_indices:', command_indices)
+    command_indices = {
+        'ctc.cli.commands': [
+            ('block',),
+            ('gas',),
+            ('transaction',),
+            ('address',),
+            ('token',),
+            ('call',),
+        ],
+        'ctc.protocols.fei_utils.cli': [('fei', 'eth')],
+        'ctc.protocols.chainlink_utils.cli': [('chainlink',)],
+    }
+
     cli = toolcli.BaseCLI(
         command_indices=command_indices,
         passthrough_command=passthrough_command,
@@ -25,6 +48,9 @@ def passthrough_command(command):
 
     elif arg.startswith('0x') and len(arg) == 42:
         return 'address'
+
+    elif str.isalnum(arg) and len(arg) <= 8:
+        return 'token'
 
     else:
         raise Exception('could not determine command')
