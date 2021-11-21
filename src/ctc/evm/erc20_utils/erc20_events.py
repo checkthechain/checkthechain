f85466e91fceb5ad34f37aeb0ea51c82b573fcbc
sslivkoff <stormslivkoff@gmail.com>
2021-11-20 23:12:49 -0800

    add function for querying erc20 balances from transfers

--- src/ctc/evm/erc20_utils/erc20_events.py
+++ src/ctc/evm/erc20_utils/erc20_events.py
@@ -1,4 +1,5 @@
 from .. import event_utils
+from . import erc20_metadata
 
 
 def get_erc20_transfers(
@@ -14,4 +15,42 @@ def get_erc20_transfers(
         end_block=end_block,
         **event_kwargs
     )
+
+
+def get_erc20_balances(transfers, block=None, dtype=float, normalize=True):
+
+    # filter block
+    if block is not None:
+        blocks = transfers.index.get_level_values('block_number').values
+        mask = blocks <= block
+        transfers = transfers[mask]
+
+    if 'arg__amount' in transfers.columns:
+        amount_key = 'arg__amount'
+    elif 'arg__value' in transfers.columns:
+        amount_key = 'arg__value'
+    else:
+        raise Exception('could not detect a transfer amout key in transfers')
+
+    # convert to float
+    transfers[amount_key] = transfers[amount_key].map(dtype)
+
+    # subtract transfers out from transfers in
+    from_transfers = transfers.groupby('arg__from')[amount_key].sum()
+    to_transfers = transfers.groupby('arg__to')[amount_key].sum()
+    balances = to_transfers.sub(from_transfers, fill_value=0)
+
+    # normalize
+    if normalize:
+        block = transfers.index[-1][0]
+        address = transfers['contract_address'].iloc[0]
+        decimals = erc20_metadata.get_erc20_decimals(token=address, block=block)
+        balances = balances / dtype('1e' + str(decimals))
+
+    # sort
+    balances = balances.sort_values(ascending=False)
+
+    balances.name = 'transfer_amount'
+
+    return balances
 
