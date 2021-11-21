7315369e3c7c29e6f632dd861933411791a4f2ee
sslivkoff <stormslivkoff@gmail.com>
2021-11-20 23:12:35 -0800

    move chunking functions to single module

--- src/ctc/evm/block_utils/block_crud.py
+++ src/ctc/evm/block_utils/block_crud.py
@@ -152,25 +152,3 @@ def normalize_block_range(start_block, end_block, contract_address=None):
 
     return (start_block, end_block)
 
-
-def get_chunks_in_range(start_block, end_block, chunk_size, trim_excess=False):
-    """break a range of blocks into chunks of a given chunk size"""
-    chunk_start_block = (start_block // chunk_size) * chunk_size
-    chunk_end_block = ((end_block // chunk_size) + 1) * chunk_size
-    chunk_bounds = list(
-        range(chunk_start_block, chunk_end_block + chunk_size, chunk_size)
-    )
-    chunks = [
-        [start, end - 1]
-        for start, end in zip(chunk_bounds[:-1], chunk_bounds[1:])
-    ]
-
-    if trim_excess:
-        if len(chunks) > 0:
-            if chunks[0][0] < start_block:
-                chunks[0] = [start_block, chunks[0][1]]
-            if chunks[-1][-1] > end_block:
-                chunks[-1] = [chunks[-1][0], end_block]
-
-    return chunks
-
