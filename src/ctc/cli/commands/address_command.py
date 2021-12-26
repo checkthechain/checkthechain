dc517f622502c1abb530424e2f81d44f48710946
sslivkoff <stormslivkoff@gmail.com>
2021-12-25 20:15:44 -0800

    expand verbose version of address cli command

--- src/ctc/cli/commands/address_command.py
+++ src/ctc/cli/commands/address_command.py
@@ -15,6 +15,8 @@ def get_command_spec():
 
 def address_command(address, verbose, **kwargs):
     max_width = os.get_terminal_size().columns
+    if verbose:
+        verbose = 2
     evm.print_address_summary(
         address=address, verbose=verbose, max_width=max_width
     )
