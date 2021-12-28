14e9544b76cf95822d461604e4c59f7448ec4480
sslivkoff <stormslivkoff@gmail.com>
2021-12-28 00:21:36 -0800

    polish ctc setup flow

--- src/ctc/config/config_data.py
+++ src/ctc/config/config_data.py
@@ -1,3 +1,4 @@
+import filecmp
 import os
 import shutil
 
@@ -15,24 +16,24 @@ def is_data_root_initialized(data_root_path: str) -> bool:
 
     data_root_path = os.path.abspath(data_root_path)
     default_data_root = get_default_data_root()
-    print('data_root_path', data_root_path)
-    print('default_data_root', default_data_root)
 
     for root, subdirs, files in os.walk(default_data_root):
-        root_relpath = os.path.relpath(root, default_data_root)
-        check_root = os.path.join(data_root_path, root_relpath)
-        print('root_relpath', root_relpath)
-        print('check_root', check_root)
+
+        if root == default_data_root:
+            check_root = data_root_path
+        else:
+            root_relpath = os.path.relpath(root, default_data_root)
+            check_root = os.path.join(data_root_path, root_relpath)
+
         for subdir in subdirs:
             subdir_path = os.path.join(check_root, subdir)
             if not os.path.isdir(subdir_path):
                 return False
         for file in files:
+            default_data_file_path = os.path.join(root, file)
             file_path = os.path.join(check_root, file)
-            if not os.path.isfile(file_path):
-                # TODO: need to check that files are EQUAL
-                return True
-    raise Exception()
+            if not filecmp.cmp(file_path, default_data_file_path):
+                return False
 
     return True
 
@@ -47,7 +48,8 @@ def initialize_data_root(
     if os.path.splitext(path)[-1] != '':
         raise Exception('must use a directory path, not a file path')
 
-    print('Creating new data root:', path)
+    print()
+    print('Will use data root:', path)
     if not os.path.isdir(path):
         if not confirm:
             print()
@@ -63,17 +65,29 @@ def initialize_data_root(
     else:
         overwritten = []
         for root, subdirs, files in os.walk(default_data_root):
-            root_relpath = os.path.relpath(root, default_data_root)
-            check_root = os.path.join(path, root_relpath)
+
+            if root == default_data_root:
+                check_root = path
+            else:
+                root_relpath = os.path.relpath(root, default_data_root)
+                check_root = os.path.join(path, root_relpath)
+
+            # root_relpath = os.path.relpath(root, default_data_root)
+            # check_root = os.path.join(path, root_relpath)
             for file in files:
                 filepath = os.path.join(check_root, file)
                 if os.path.isfile(filepath):
-                    overwritten.append(os.path.relpath(filepath, path))
+                    # overwritten.append(os.path.relpath(filepath, path))
+                    overwritten.append(filepath)
         if len(overwritten) > 0:
+            print()
             print('Will overwrite the following files:')
             for filepath in overwritten:
-                print(filepath)
-            answer = toolcli.input_yes_or_no('Continue? ', default='yes')
+                print('-', filepath)
+            print()
+            answer = toolcli.input_yes_or_no(
+                'Continue? ', default='yes', default_prefix='(default = '
+            )
             if not answer:
                 if raise_if_unconfirmed:
                     raise Exception('Must overwrite files to continue')
@@ -81,7 +95,6 @@ def initialize_data_root(
                     return False
 
     # create directory
-    shutil.copytree(default_data_root, path)
-    print("FUCL")
+    shutil.copytree(default_data_root, path, dirs_exist_ok=True)
     return True
 
--- src/ctc/config/setup_utils.py
+++ src/ctc/config/setup_utils.py
@@ -1,9 +1,11 @@
 import json
 import os
+import sys
 
 import toolcli
 
 from ctc import spec
+from . import config_data
 from . import config_spec
 from . import config_utils
 
@@ -23,33 +25,39 @@ def setup_ctc() -> None:
     # collect new config file data
     config_path, create_because_config_path = setup_config_path()
     data_root, create_because_data_root = setup_data_root()
-    providers, create_because_provider = setup_providers()
-    default_network, create_because_default_network = setup_default_network()
-    network_metadata, create_because_network_metadata = setup_network_metadata()
+    # providers, create_because_provider = setup_providers()
+    # default_network, create_because_default_network = setup_default_network()
+    # network_metadata, create_because_network_metadata = setup_network_metadata()
 
     # create new config file if need be
     create_new_config = any(
-        create_because_config_path,
-        create_because_data_root,
-        create_because_provider,
-        create_because_network_metadata,
-        create_because_default_network,
+        [
+            create_because_config_path,
+            create_because_data_root,
+            # create_because_provider,
+            # create_because_network_metadata,
+            # create_because_default_network,
+        ]
     )
+
+    print()
+    print()
+    print('## Final Steps')
+
     if create_new_config:
         config = {
             'data_root': data_root,
-            'providers': providers,
-            'default_network': default_network,
-            'network_metadata': network_metadata,
+            # 'providers': providers,
+            # 'default_network': default_network,
+            # 'network_metadata': network_metadata,
         }
         with open(config_path, 'w') as f:
             json.dump(config, f)
         print()
-        print('config file created at:', config_path)
+        print('Config file created at:', config_path)
 
     # remind user to set env var
     if os.environ.get(config_spec.config_path_env_var) != config_path:
-        print()
         print()
         print(
             'Remember to set your',
@@ -57,7 +65,6 @@ def setup_ctc() -> None:
             'environment variable to',
             config_path,
         )
-        print()
         print(
             'For example, if using bash you can add the following line to ~/.profile:'
         )
@@ -84,19 +91,21 @@ def setup_config_path() -> tuple[str, bool]:
         os.path.join('~', '.config', 'ctc')
     )
 
+    input_kwargs: toolcli.InputFilenameOrDirectoryKwargs = {
+        'prompt': 'Where should config be created? Specify a file path or directory path',
+        'default_directory': default_config_path,
+        'default_filename': 'config.json',
+    }
+
     print()
     print()
     print('## Config Path')
     print()
 
     if not config_utils.config_path_is_set():
-        print('Config path not set in', env_var)
+        print('Config path not currently set in', env_var)
         print()
-        prompt = 'Where should config be created?'
-        new_config_path = toolcli.input_prompt(
-            prompt=prompt,
-            default=default_config_path,
-        )
+        new_config_path = toolcli.input_filename_or_directory(**input_kwargs)
         create_new_config = True
 
     elif not config_utils.config_path_exists():
@@ -109,11 +118,7 @@ def setup_config_path() -> tuple[str, bool]:
         if answer:
             new_config_path = config_path
         else:
-            prompt = 'Where should config be created?'
-            new_config_path = toolcli.input_prompt(
-                prompt=prompt,
-                default=default_config_path,
-            )
+            new_config_path = toolcli.input_filename_or_directory(**input_kwargs)
         create_new_config = True
 
     else:
@@ -126,11 +131,7 @@ def setup_config_path() -> tuple[str, bool]:
             new_config_path = config_path
             create_new_config = False
         else:
-            prompt = 'Where should config be created?'
-            new_config_path = toolcli.input_prompt(
-                prompt=prompt,
-                default=default_config_path,
-            )
+            new_config_path = toolcli.input_filename_or_directory(**input_kwargs)
             create_new_config = True
 
     # check file extension
@@ -170,8 +171,9 @@ def setup_data_root() -> tuple[str, bool]:
     # data_root
     if not config_utils.config_path_exists():
         print()
-        new_data_root = toolcli.input_prompt(
-            prompt='Where should ctc store data? (specify a directory)\n'
+        new_data_root = toolcli.input_filename_or_directory(
+            prompt='Where should ctc store data? (specify a directory path)\n',
+            create_directory='prompt_and_require',
         )
         create_new_config = True
 
@@ -183,8 +185,10 @@ def setup_data_root() -> tuple[str, bool]:
 
         if not isinstance(data_root, str):
             print('This value is invalid')
-            print('Where should ctc store data? (specify a directory)')
-            new_data_root = input()
+            new_data_root = toolcli.input_filename_or_directory(
+                prompt='Where should ctc store data? (specify a directory path)\n',
+                create_directory='prompt_and_require',
+            )
             create_new_config = True
 
         elif os.path.abspath(data_root) != data_root:
@@ -195,8 +199,9 @@ def setup_data_root() -> tuple[str, bool]:
                 new_data_root = data_root
                 create_new_config = False
             else:
-                new_data_root = toolcli.get_input_directory(
-                    'Where should ctc store data? (specify a directory)'
+                new_data_root = toolcli.input_filename_or_directory(
+                    prompt='Where should ctc store data? (specify a directory path)\n',
+                    create_directory='prompt_and_require',
                 )
                 create_new_config = True
 
@@ -204,68 +209,67 @@ def setup_data_root() -> tuple[str, bool]:
             if toolcli.input_yes_or_no('Keep storing data in this directory?'):
                 new_data_root = data_root
             else:
-                new_data_root = toolcli.get_input_directory(
-                    'Where should ctc store data? (specify a directory)'
+                new_data_root = toolcli.input_filename_or_directory(
+                    prompt='Where should ctc store data? (specify a directory path)\n',
+                    create_directory='prompt_and_require',
                 )
 
-    # create directory if does not exist
-    if not os.path.isdir(new_data_root):
-        if toolcli.input_yes_or_no('Data directory does not exist, create it?'):
-
-
     # initialize directory data
-    data_root_path = os.path.join(new_data_root, 'data_root.json')
-    if not os.path.isfile(data_root_path):
-        print('Initializing directory data')
-        populate_initial_data(data_root=new_data_root)
+    if config_data.is_data_root_initialized(new_data_root):
+        print()
+        print('Data directory is already initialized')
+    else:
+        initialized = config_data.initialize_data_root(new_data_root)
+        if not initialized:
+            return setup_data_root()
 
     return new_data_root, create_new_config
 
 
-def populate_initial_data():
-    metadata = {
-        'data_root_version': 0.1,
-    }
+def get_default_data_root():
+    import ctc
+
+    return os.path.join(ctc.__path__, 'default_data')
 
 
-def setup_providers() -> tuple[
-    dict[spec.NetworkReference, spec.ProviderSpec], bool
-]:
-    # TODO: check if networks are known and valid
-    if config_utils.config_exists():
-        config = config_utils.get_config()
-    else:
-        config = None
+# def setup_providers() -> tuple[
+#     dict[spec.NetworkReference, spec.ProviderSpec], bool
+# ]:
+#     # TODO: check if networks are known and valid
+#     if config_utils.config_exists():
+#         config = config_utils.get_config()
+#     else:
+#         config = None
 
-    if config is not None:
-        old_providers = config.get('providers')
-        if not isinstance(old_providers, list):
-            print()
-            print('Current config value of providers is invaild')
+#     if config is not None:
+#         old_providers = config.get('providers')
+#         if not isinstance(old_providers, list):
+#             print()
+#             print('Current config value of providers is invaild')
 
-        if len(providers) > 0:
-            print()
-            print('Current providers in config:')
-            for network, provider in config['providers'].items():
-                print('-', network + ':', provider)
-        else:
-            print()
-            print('No providers currently specified in config')
-    else:
-        print('Would you like to add a provider?')
-        print()
+#         if len(providers) > 0:
+#             print()
+#             print('Current providers in config:')
+#             for network, provider in config['providers'].items():
+#                 print('-', network + ':', provider)
+#         else:
+#             print()
+#             print('No providers currently specified in config')
+#     else:
+#         print('Would you like to add a provider?')
+#         print()
 
-    print()
-    print('Would you like to add more providers? ')
-
-
-def setup_default_network() -> tuple[spec.NetworkName, bool]:
-    if config_utils.config_exists():
-        config = config_utils.get_config()
-    else:
-        config = None
+#     print()
+#     print('Would you like to add more providers? ')
 
 
-def setup_network_metadata():
-    pass
+# def setup_default_network() -> tuple[spec.NetworkName, bool]:
+#     if config_utils.config_exists():
+#         config = config_utils.get_config()
+#     else:
+#         config = None
+
+
+# def setup_network_metadata() -> bool:
+#     pass
 
