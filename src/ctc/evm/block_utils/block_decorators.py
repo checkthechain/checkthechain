d1a279d1a11ae2752c97bd0fda636d904593d064
sslivkoff <stormslivkoff@gmail.com>
2021-12-29 00:55:50 -0800

    remove parallel config kwarg checking in old code

--- src/ctc/evm/block_utils/block_decorators.py
+++ src/ctc/evm/block_utils/block_decorators.py
@@ -5,7 +5,8 @@ from ctc import config_utils
 
 def parallelize_block_fetching(config=None, **kwargs):
 
-    parallel_fetch_config = config_utils.get_config()['parallel_fetch_config']
+    # parallel_fetch_config = config_utils.get_config()['parallel_fetch_config']
+    parallel_fetch_config = {}
     if config is None:
         config = {}
     config = dict(parallel_fetch_config, **config)
