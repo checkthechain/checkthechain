15c78c82823b7e8fd56f463964355db630082ac4
sslivkoff <stormslivkoff@gmail.com>
2022-01-24 22:47:03 -0800

    handle edge cases in bin_by_blocks

--- src/ctc/evm/block_utils/block_analysis.py
+++ src/ctc/evm/block_utils/block_analysis.py
@@ -3,11 +3,14 @@ import typing
 from ctc import spec
 
 
+T = typing.TypeVar('T', spec.DataFrame, spec.Series)
+
+
 def bin_by_blocks(
-    data: spec.DataFrame,
+    data: T,
     blocks: typing.Sequence[typing.SupportsInt],
-    block_index_name: typing.Optional[str]='block_number',
-) -> spec.DataFrame:
+    block_index_name: typing.Optional[str] = 'block_number',
+) -> T:
     """TODO: deprecate
     - either:
         - refactor using pd.cut() https://stackoverflow.com/a/33761120
@@ -19,15 +22,17 @@ def bin_by_blocks(
     if block_index_name is not None and len(data.index.names) > 1:
         for index_name in data.index.names:
             if index_name != 'block_number':
-                data = typing.cast(spec.DataFrame, data.droplevel(index_name))
+                data.index = data.index.droplevel(index_name)
 
     standard_blocks = [int(block) for block in blocks]
     data = data.groupby(np.digitize(data.index.values, standard_blocks)).sum()
     data = pd.Series([0] * len(blocks), index=range(1, len(blocks) + 1)).add(
         data, fill_value=0
     )
-    data.index = standard_blocks
-    data.index.name = 'after_this_block'  # type: ignore
+    if data.index[0] < standard_blocks[0]:
+        data = data.iloc[1:]
+    data.index = pd.Index(standard_blocks)
+    data.index.name = 'gte_this_block'  # type: ignore
 
     return data
 
