546b7d1d52b7a8a422934f0c6c6f05d570bd80eb
sslivkoff <stormslivkoff@gmail.com>
2022-02-02 12:15:14 -0800

    update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,33 @@ setuptools.setup(
     packages=setuptools.find_packages("./src"),
     package_dir={'': 'src'},
     install_requires=[
-        'boto3',
-        'fsspec',
-        's3fs',
+        #
+        # data science
         'matplotlib',
         'numpy',
         'pandas',
-        'tooltime',
-        'toolcache',
-        'pyyaml',
-        'requests',
-        'pysha3',  # for sha3
         'pandas-stubs',
+        #
+        # data dependencies
+        'aiohttp',
+        'aiofiles',
+        'pyyaml',
+        'toml',
+        #
+        # tool suite
+        'toolcache',
+        'toolcli',
+        'toolconfig',
+        'toolplot',
+        'toolstr',
+        'tooltable',
+        'tooltime',
+        #
+        # EVM dependencies
+        'pysha3',  # for keccak()
+        'eth_abi',  # for encode_single()/decode_single()
+        'eth_utils',  # for collapse_if_tuple()
+        'rlp',  # for create2 address computation
     ],
     scripts=[
         './scripts/ctc'
