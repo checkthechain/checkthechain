8615d762058ae4abacfd60b02984be8edc79861b
sslivkoff <stormslivkoff@gmail.com>
2022-02-02 12:14:53 -0800

    fix typing in filesystem_utils

--- src/ctc/toolbox/filesystem_utils/filesystem_dirsize.py
+++ src/ctc/toolbox/filesystem_utils/filesystem_dirsize.py
@@ -3,15 +3,15 @@ import subprocess
 
 def get_directory_nbytes(dirpath: str) -> int:
     cmd = 'du -sb ' + dirpath
-    cmd = cmd.split(' ')
-    output = subprocess.check_output(cmd)
+    cmd_pieces = cmd.split(' ')
+    output = subprocess.check_output(cmd_pieces)
     output = output.split(b'\t')[0].decode()
     return int(output)
 
 
 def get_directory_nbytes_human(dirpath: str) -> str:
     cmd = 'du -h ' + dirpath
-    cmd = cmd.split(' ')
-    output = subprocess.check_output(cmd)
+    cmd_pieces = cmd.split(' ')
+    output = subprocess.check_output(cmd_pieces)
     return output.split(b'\t')[0].decode()
 
