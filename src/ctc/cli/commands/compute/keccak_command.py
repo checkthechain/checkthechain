442bea569895e954abeae5e4d44520cd654137ba
sslivkoff <stormslivkoff@gmail.com>
2022-02-13 00:01:53 -0800

    improve keccak cli command for both text and hex

--- src/ctc/cli/commands/compute/keccak_command.py
+++ src/ctc/cli/commands/compute/keccak_command.py
@@ -7,14 +7,22 @@ def get_command_spec():
         'args': [
             {'name': 'data'},
             {'name': '--text', 'kwargs': {'action': 'store_true'}},
+            {'name': '--hex', 'kwargs': {'action': 'store_true'}},
         ],
     }
 
 
-def keccack_command(data, text):
+def keccack_command(data, text, hex):
     if text:
-        keccak = binary.keccak_text(data)
+        hex = False
+    elif hex:
+        pass
     else:
+        hex = data.startswith('0x')
+
+    if hex:
         keccak = binary.keccak(data)
+    else:
+        keccak = binary.keccak_text(data)
     print(keccak)
 
