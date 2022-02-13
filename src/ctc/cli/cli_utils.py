689989ee0b7e90b63013076ffd7cea938535bc43
sslivkoff <stormslivkoff@gmail.com>
2022-02-13 00:01:05 -0800

    add cli utils for new block notation and data output options

---
+++ src/ctc/cli/cli_utils.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+
+import os
+import typing
+
+import toolcli
+
+from ctc import evm
+
+
+async def async_resolve_blocks(
+    block_spec: typing.Union[str, list[str]]
+) -> list[int]:
+
+    # convert to tokens
+    if isinstance(block_spec, str):
+        tokens = [block_spec]
+    else:
+        tokens = [
+            subtoken
+            for token in block_spec
+            for subtoken in token.strip(',').split(',')
+        ]
+
+    if len(tokens) == 0:
+        raise Exception('invalid block specification: ' + str(block_spec))
+
+    start_range = tokens[0][0] in ['[', '(']
+    end_range = tokens[-1][-1] in [']', ')']
+
+    if (start_range and not end_range) or (not start_range and end_range):
+        raise Exception('invalid block specification: ' + str(block_spec))
+
+    if start_range and end_range:
+
+        open_start = tokens[0][0] == '('
+        open_end = tokens[-1][-1] == ')'
+        stripped = [token.strip('()[] \t') for token in tokens]
+
+        if len(tokens) == 1:
+            if open_start or open_end:
+                raise Exception(
+                    'invalid block specification: ' + str(block_spec)
+                )
+            blocks = [stripped[0]]
+
+        else:
+            start_block, end_block = await evm.async_block_numbers_to_int(
+                stripped[:2]
+            )
+            if not open_start:
+                start_block += 1
+
+            if len(tokens) == 2:
+                blocks = list(range(start_block, end_block))
+            elif len(tokens) == 3:
+                block_interval = int(stripped[2])
+                blocks = list(range(start_block, end_block, block_interval))
+            else:
+                raise Exception(
+                    'invalid block specification: ' + str(block_spec)
+                )
+
+            if not open_end:
+                blocks.append(end_block)
+
+            else:
+                raise Exception(
+                    'invalid block specification: ' + str(block_spec)
+                )
+
+    else:
+        blocks = tokens
+
+    return await evm.async_block_numbers_to_int(blocks=blocks)
+
+
+def output_data(data, output, overwrite):
+
+    if output == 'stdout':
+        # print(data)
+        import tooltable
+        import toolstr
+        rows = []
+        for index, values in data.iterrows():
+            row = []
+            row.append(index)
+            for value in values.values:
+                if value and not isinstance(value, str):
+                    value = toolstr.format(value)
+                row.append(value)
+            rows.append(row)
+        columns = [data.index.name] + list(data.columns)
+        tooltable.print_table(rows=rows, headers=columns)
+
+    else:
+
+        # check whether file exists
+        if os.path.isfile(output):
+            if overwrite:
+                pass
+            elif toolcli.input_yes_or_no('File already exists. Overwrite? '):
+                pass
+            else:
+                raise Exception('aborting')
+
+        if output.endswith('.csv'):
+            data.to_csv(output)
+
+        elif output.endswith('.json'):
+            data.to_json(output)
+
+        else:
+            raise Exception('unknown output format: ' + str(output))
+
