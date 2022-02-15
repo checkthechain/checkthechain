eafe1c5212f75ace0765fa66ab0cde44a10051fb
sslivkoff <stormslivkoff@gmail.com>
2022-02-14 16:41:49 -0800

    bump to 0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='checkthechain',
-    version='0.2.2',
+    version='0.2.3',
     packages=setuptools.find_packages("./src"),
     package_dir={'': 'src'},
     package_data={'ctc': ['default_data/*']},
