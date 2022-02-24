24f734e0a09da418e46aa5e732892322ec88844f
sslivkoff <stormslivkoff@gmail.com>
2022-02-23 23:57:44 -0800

    add balancer resource notes

--- src/ctc/protocols/balancer_utils/pool_trades.py
+++ src/ctc/protocols/balancer_utils/pool_trades.py
@@ -1,3 +1,9 @@
+"""
+## Resources
+- https://github.com/TokenEngineeringCommunity/BalancerPools_Model
+- https://token-engineering-balancer.gitbook.io/balancer-simulations/additional-code-and-instructions/balancer-the-python-edition/balancer_math.py
+"""
+
 
 def calc_out_given_in(
     token_amount_in,
@@ -7,6 +13,9 @@ def calc_out_given_in(
     token_weight_out,
     swap_fee,
 ):
+    """
+    adapted from https://github.com/TokenEngineeringCommunity/BalancerPools_Model
+    """
     weight_ratio = token_weight_in / token_weight_out
     adjusted_in = token_amount_in * (1 - swap_fee)
     y = token_balance_in / (token_balance_in + adjusted_in)
