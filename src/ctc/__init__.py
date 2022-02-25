a3e0367bc6714877995915a1717b9f5851b64470
sslivkoff <stormslivkoff@gmail.com>
2022-02-24 18:14:22 -0800

    bump to 0.2.6

--- src/ctc/__init__.py
+++ src/ctc/__init__.py
@@ -1,5 +1,5 @@
 # TODO: set __version__ in setup.cfg for single source of truth
 # - https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
 # - using importlib.metadata add too much to startup time, not good for cli's
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
