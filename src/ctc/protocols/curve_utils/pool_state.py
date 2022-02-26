c6600be604f0ed96d1f0356f77c3fec03ac38d99
sslivkoff <stormslivkoff@gmail.com>
2022-02-26 09:31:45 -0800

    add get_trade to curve_utils

--- src/ctc/protocols/curve_utils/pool_state.py
+++ src/ctc/protocols/curve_utils/pool_state.py
@@ -1,5 +1,8 @@
+import typing
+
 from ctc import evm
 from ctc import rpc
+from ctc import spec
 
 from . import pool_metadata
 
@@ -54,4 +57,55 @@ async def async_get_pool_state(pool, n_tokens=None, block=None, provider=None):
         'lp_total_supply': total_supply,
         'token_balances': token_balances,
     }
+
+
+async def async_get_trade(
+    pool: spec.Address,
+    token_in: typing.Union[int, spec.Address],
+    token_out: typing.Union[int, spec.Address],
+    amount_in: typing.Union[int, float],
+    input_normalized: bool = True,
+    normalize_output: bool = True,
+    provider: spec.ProviderSpec = None,
+    block: typing.Optional[spec.BlockNumberReference] = None,
+) -> typing.Union[int, float]:
+
+    # get metadata
+    metadata = await pool_metadata.async_get_pool_metadata(
+        pool=pool,
+        provider=provider,
+    )
+    in_index = await pool_metadata.async_get_token_index(
+        pool=pool,
+        token=token_in,
+        metadata=metadata,
+    )
+    out_index = await pool_metadata.async_get_token_index(
+        pool=pool,
+        token=token_out,
+        metadata=metadata,
+    )
+
+    # denormalize input
+    if input_normalized:
+        amount_in *= 10 ** metadata['token_decimals'][in_index]
+
+    # query data
+    amount_out = await rpc.async_eth_call(
+        to_address=pool,
+        function_name='get_dy',
+        function_parameters=[
+            in_index,
+            out_index,
+            amount_in,
+        ],
+        provider=provider,
+        block_number=block,
+    )
+
+    # normalize output
+    if normalize_output:
+        amount_out = amount_out / (10 ** metadata['token_decimals'][out_index])
+
+    return amount_out
 
