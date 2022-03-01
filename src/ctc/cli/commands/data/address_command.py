b1423025e87a6f65a7dcb4528072e826b2d6f86c
sslivkoff <stormslivkoff@gmail.com>
2022-02-28 16:53:47 -0800

    add network option to address inspection command

--- src/ctc/cli/commands/data/address_command.py
+++ src/ctc/cli/commands/data/address_command.py
@@ -11,16 +11,20 @@ def get_command_spec():
         'args': [
             {'name': 'address'},
             {'name': ['-v', '--verbose'], 'action': 'store_true'},
+            {'name': '--network'},
         ],
     }
 
 
-async def async_address_command(address, verbose, **kwargs):
+async def async_address_command(address, verbose, network, **kwargs):
     max_width = os.get_terminal_size().columns
     if verbose:
         verbose = 2
     await evm.async_print_address_summary(
-        address=address, verbose=verbose, max_width=max_width
+        address=address,
+        verbose=verbose,
+        max_width=max_width,
+        provider={'network': network},
     )
     await rpc.async_close_http_session()
 
