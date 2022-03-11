008c8ba3faf5bb68224c04607f979506621bc48e
sslivkoff <stormslivkoff@gmail.com>
2022-03-10 18:44:36 -0800

    fix help message of root command

--- src/ctc/cli/commands/root_command.py
+++ src/ctc/cli/commands/root_command.py
@@ -4,19 +4,17 @@ import toolcli
 def get_command_spec():
     return {
         'f': root_command,
-        'args': [
-            {'name': ['-h', '--help'], 'help': 'print help message', 'action': 'store_true'},
-        ],
+        'help': 'cli interface to ctc and its subcommands',
         'special': {
             'include_parse_spec': True,
         },
     }
 
 
-def root_command(parse_spec, help):
+def root_command(parse_spec):
     toolcli.execute_other_command_sequence(
         command_sequence=('help',),
-        args=[],
+        args={'parse_spec': parse_spec},
         parse_spec=parse_spec,
     )
 
