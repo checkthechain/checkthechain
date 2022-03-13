9e496bec2764445e9c94eed3732c1f75df96794d
sslivkoff <stormslivkoff@gmail.com>
2022-03-12 21:14:54 -0800

    fix contract address checking

--- src/ctc/evm/address_utils/address_queries.py
+++ src/ctc/evm/address_utils/address_queries.py
@@ -5,13 +5,12 @@ async def async_is_contract_address(address, block='latest', provider=None):
     code = await rpc.async_eth_get_code(
         address=address, block_number=block, provider=provider,
     )
-    return len(code) > 3
+    return len(code) >= 3
 
 
 async def async_are_contract_addresses(addresses, block='latest', provider=None):
     codes = await rpc.async_batch_eth_get_code(
         addresses=addresses, block_number=block, provider=provider,
     )
-    block = list(codes.keys())[0]
-    return {address: len(code) > 3 for address, code in codes[block].items()}
+    return {address: len(code) > 3 for address, code in zip(addresses, codes)}
 
