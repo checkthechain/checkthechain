e01c597714ea968da8e7bea7964c07ff2ef39444
sslivkoff <stormslivkoff@gmail.com>
2022-03-12 23:22:37 -0800

    revamp uniswap v2 event parsing

--- src/ctc/protocols/uniswap_v2_utils/uniswap_v2_events.py
+++ src/ctc/protocols/uniswap_v2_utils/uniswap_v2_events.py
@@ -18,12 +18,13 @@ async def async_get_pool_swaps(
     provider: spec.ProviderSpec = None,
 ) -> spec.DataFrame:
 
-    if normalize or replace_symbols:
+    if replace_symbols:
         symbols_task = asyncio.create_task(
             uniswap_v2_metadata.async_get_pool_symbols(
                 pool_address, provider=provider
             )
         )
+    if normalize:
         decimals_task = asyncio.create_task(
             uniswap_v2_metadata.async_get_pool_decimals(
                 pool_address, provider=provider
@@ -37,14 +38,34 @@ async def async_get_pool_swaps(
         end_block=end_block,
         provider=provider,
     )
+    swaps['arg__amount0In'] = swaps['arg__amount0In'].map(int)
+    swaps['arg__amount0Out'] = swaps['arg__amount0Out'].map(int)
+    swaps['arg__amount1In'] = swaps['arg__amount1In'].map(int)
+    swaps['arg__amount1Out'] = swaps['arg__amount1Out'].map(int)
 
-    if normalize or replace_symbols:
-        symbols = await symbols_task
-        decimals = await decimals_task
+    # normalize columns
+    if normalize:
+        x_decimals, y_decimals = await decimals_task
+        swaps['arg__amount0In'] = await evm.async_normalize_erc20_quantities(
+            quantities=swaps['arg__amount0In'].astype(float),
+            decimals=x_decimals,
+        )
+        swaps['arg__amount0Out'] = await evm.async_normalize_erc20_quantities(
+            quantities=swaps['arg__amount0Out'].astype(float),
+            decimals=x_decimals,
+        )
+        swaps['arg__amount1In'] = await evm.async_normalize_erc20_quantities(
+            quantities=swaps['arg__amount1In'].astype(float),
+            decimals=y_decimals,
+        )
+        swaps['arg__amount1Out'] = await evm.async_normalize_erc20_quantities(
+            quantities=swaps['arg__amount1Out'].astype(float),
+            decimals=y_decimals,
+        )
 
     # rename columns
     if replace_symbols:
-        x_symbol, y_symbol = symbols
+        x_symbol, y_symbol = await symbols_task
     else:
         x_symbol = 'x'
         y_symbol = 'y'
@@ -56,22 +77,6 @@ async def async_get_pool_swaps(
     }
     swaps = swaps.rename(columns=columns)
 
-    # normalize columns
-    if normalize:
-        x_decimals, y_decimals = decimals
-        swaps[columns['arg__amount0In']] = swaps[
-            columns['arg__amount0In']
-        ].astype(float) / (10 ** x_decimals)
-        swaps[columns['arg__amount0Out']] = swaps[
-            columns['arg__amount0Out']
-        ].astype(float) / (10 ** x_decimals)
-        swaps[columns['arg__amount1In']] = swaps[
-            columns['arg__amount1In']
-        ].astype(float) / (10 ** y_decimals)
-        swaps[columns['arg__amount1Out']] = swaps[
-            columns['arg__amount1Out']
-        ].astype(float) / (10 ** y_decimals)
-
     return swaps
 
 
@@ -79,28 +84,109 @@ async def async_get_pool_mints(
     pool_address,
     start_block=None,
     end_block=None,
-    # replace_symbols=False,
-    # normalize=True,
+    replace_symbols=False,
+    normalize=True,
+    provider=None,
 ):
-    return await evm.async_get_events(
+    if normalize:
+        decimals_task = asyncio.create_task(
+            uniswap_v2_metadata.async_get_pool_decimals(
+                pool_address, provider=provider
+            )
+        )
+    if replace_symbols:
+        symbols_task = asyncio.create_task(
+            uniswap_v2_metadata.async_get_pool_symbols(
+                pool_address, provider=provider
+            )
+        )
+
+    mints = await evm.async_get_events(
         event_name='Mint',
         contract_address=pool_address,
         start_block=start_block,
         end_block=end_block,
+        provider=provider,
     )
+    mints['arg__amount0'] = mints['arg__amount0'].map(int)
+    mints['arg__amount1'] = mints['arg__amount1'].map(int)
+
+    if normalize:
+        decimals0, decimals1 = await decimals_task
+        mints['arg__amount0'] = await evm.async_normalize_erc20_quantities(
+            quantities=mints['arg__amount0'].astype(float),
+            decimals=decimals0,
+            provider=provider,
+        )
+        mints['arg__amount1'] = await evm.async_normalize_erc20_quantities(
+            quantities=mints['arg__amount1'].astype(float),
+            decimals=decimals1,
+            provider=provider,
+        )
+
+    if replace_symbols:
+        symbol0, symbol1 = await symbols_task
+        new_names = {
+            'arg__amount0': symbol0 + '_amount',
+            'arg__amount1': symbol1 + '_amount',
+        }
+        mints = mints.rename(columns=new_names)
+
+    return mints
 
 
 async def async_get_pool_burns(
     pool_address,
     start_block=None,
     end_block=None,
-    # replace_symbols=False,
-    # normalize=True,
+    replace_symbols=False,
+    normalize=True,
+    provider=None,
 ):
-    return await evm.async_get_events(
+
+    if normalize:
+        decimals_task = asyncio.create_task(
+            uniswap_v2_metadata.async_get_pool_decimals(
+                pool_address, provider=provider
+            )
+        )
+    if replace_symbols:
+        symbols_task = asyncio.create_task(
+            uniswap_v2_metadata.async_get_pool_symbols(
+                pool_address, provider=provider
+            )
+        )
+
+    burns = await evm.async_get_events(
         event_name='Burn',
         contract_address=pool_address,
         start_block=start_block,
         end_block=end_block,
+        provider=provider,
     )
+    burns['arg__amount0'] = burns['arg__amount0'].map(int)
+    burns['arg__amount1'] = burns['arg__amount1'].map(int)
+
+    if normalize:
+        decimals0, decimals1 = await decimals_task
+        burns['arg__amount0'] = await evm.async_normalize_erc20_quantities(
+            quantities=burns['arg__amount0'].astype(float),
+            decimals=decimals0,
+            provider=provider,
+        )
+        burns['arg__amount1'] = await evm.async_normalize_erc20_quantities(
+            quantities=burns['arg__amount1'].astype(float),
+            decimals=decimals1,
+            provider=provider,
+        )
+
+    if replace_symbols:
+        symbol0, symbol1 = await symbols_task
+        new_names = {
+            'arg__amount0': symbol0 + '_amount',
+            'arg__amount1': symbol1 + '_amount',
+        }
+        burns = burns.rename(columns=new_names)
+
+    return burns
 
