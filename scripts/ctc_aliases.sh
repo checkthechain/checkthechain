8de371eb61169f4d5f80836c866392b5ea1d05db
sslivkoff <stormslivkoff@gmail.com>
2022-03-15 16:14:32 -0700

    add script to set cli aliases

---
+++ scripts/ctc_aliases.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# these commands allow you to call ctc subcommands without typing the "ctc" part
+
+# to use, either:
+# 1. add the contents of this file to the end of your terminal config file
+# 2. add `source PATH_TO_THIS_FILE` to the end of your terminal config file
+
+# to learn more about these commands, run `ctc help`
+
+# depending on preference can use all of these aliases or just a subset of them:
+
+# compute commands
+alias ascii="ctc ascii"
+alias hex="ctc hex"
+alias keccak="ctc keccack"
+alias lower="ctc lower"
+
+# data commands
+alias address="ctc address"
+alias block="ctc block"
+alias blocks="ctc blocks"
+alias call="ctc call"
+alias calls="ctc calls"
+alias eth="ctc eth"
+alias erc20="ctc erc20"
+alias transaction="ctc transaction"
+alias gas="ctc gas"
+
+# protocol commands
+alias 4byte="ctc 4byte"
+alias cg="ctc coingecko"
+alias chainlink="ctc chainlink"
+alias ens="ctc ens"
+alias fei="ctc fei"
+alias rari="ctc rari"
+
