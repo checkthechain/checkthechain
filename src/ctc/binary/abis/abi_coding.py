bf061f211681173366946a886c97211da33152fd
sslivkoff <stormslivkoff@gmail.com>
2022-03-17 15:44:51 -0700

    add note about desired abi coding agnostic features

--- src/ctc/binary/abis/abi_coding.py
+++ src/ctc/binary/abis/abi_coding.py
@@ -6,12 +6,14 @@ from ctc import spec
 
 
 def abi_decode(data: bytes, types: spec.ABIDatatypeStr) -> typing.Any:
+    # TODO: allow for any input format, allow specifying any output format
     import eth_abi
 
     return eth_abi.decode_single(types, data)
 
 
 def abi_encode(data: typing.Any, types: spec.ABIDatatypeStr) -> bytes:
+    # TODO: allow for any input format, allow specifying any output format
     import eth_abi
 
     return eth_abi.encode_single(types, data)
