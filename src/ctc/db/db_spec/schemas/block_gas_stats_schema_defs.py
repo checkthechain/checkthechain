27d6f50429d04c868f8afac7abb819028fba830c
sslivkoff <stormslivkoff@gmail.com>
2022-03-31 13:51:36 -0700

    create block gas stats schema

---
+++ src/ctc/db/db_spec/schemas/block_gas_stats_schema_defs.py
@@ -0,0 +1,28 @@
+import toolsql
+
+block_gas_stats_schema: toolsql.DBSchema = {
+    'tables': {
+        'block_gas_stats': {
+            'columns': [
+                {'name': 'block_number', 'type': 'Integer', 'primary': True},
+                {'name': 'base', 'type': 'Integer'},
+                {'name': 'mean', 'type': 'Integer'},
+                {'name': 'median', 'type': 'Integer'},
+                {'name': 'min', 'type': 'Integer'},
+                {'name': 'max', 'type': 'Integer'},
+                {'name': 'p1', 'type': 'Integer'},
+                {'name': 'p5', 'type': 'Integer'},
+                {'name': 'p10', 'type': 'Integer'},
+                {'name': 'p25', 'type': 'Integer'},
+                {'name': 'p75', 'type': 'Integer'},
+                {'name': 'p90', 'type': 'Integer'},
+                {'name': 'p95', 'type': 'Integer'},
+                {'name': 'p99', 'type': 'Integer'},
+                {'name': 'limit', 'type': 'Integer'},
+                {'name': 'used', 'type': 'Integer'},
+                {'name': 'n_transactions', 'type': 'Integer'},
+            ],
+        },
+    },
+}
+
