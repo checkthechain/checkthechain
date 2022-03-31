7b339fa114694cf09dcdd5f7377c04b5b9768714
sslivkoff <stormslivkoff@gmail.com>
2022-03-30 22:19:28 -0700

    expand config validation logic and drop warnings when format is improper

--- src/ctc/config/config_read.py
+++ src/ctc/config/config_read.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import typing
+import warnings
 from typing_extensions import TypedDict
 
 import toolconfig
@@ -41,16 +42,50 @@ def get_config(
 
 
 def get_config(
-    validate: toolconfig.ValidationOption = 'raise',
+    validate: toolconfig.ValidationOption = False,
 ) -> typing.Union[spec.ConfigSpec, typing.MutableMapping]:
 
     config = toolconfig.get_config(
         config_spec=spec.ConfigSpec, validate=validate, **_kwargs
     )
+
+    validation = validate_config(config)
+    if not validation['valid']:
+        warning_text = '\n** ATTENTION **\nctc config is not formatted correctly'
+        if len(validation['missing_keys']) > 0:
+            warning_text += '\n    missing keys: ' + ', '.join(
+                str(key) for key in validation['missing_keys']
+            )
+        if len(validation['extra_keys']) > 0:
+            warning_text += '\n    extra keys: ' + ', '.join(
+                str(key) for key in validation['extra_keys']
+            )
+        warning_text += '\nview more config info by running `[#64aaaa]ctc config[/#64aaaa]` in terminal'
+        warning_text += '\nfix config by running `[#64aaaa]ctc setup[/#64aaaa]` in terminal'
+        warning_text += '\n'
+
+        import rich.console
+        warning_text = '[yellow]' + warning_text + '[/yellow]'
+        console = rich.console.Console()
+        console.print(warning_text)
+
     if validate == 'raise':
         return typing.cast(spec.ConfigSpec, config)
     else:
         return config
+
+
+def validate_config(config):
+    spec_keys = set(spec.ConfigSpec.__annotations__)
+    actual_keys = set(config.keys())
+    missing_keys = spec_keys - actual_keys
+    extra_keys = actual_keys - spec_keys
+    valid = spec_keys == actual_keys
+    return {
+        'valid': valid,
+        'missing_keys': missing_keys,
+        'extra_keys': extra_keys,
+    }
 
 
 def config_is_valid() -> bool:
