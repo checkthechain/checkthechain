954b542b41a5c67ee4e47cbfe707f7c1ba1ec0e8
sslivkoff <stormslivkoff@gmail.com>
2022-04-02 14:24:42 -0700

    hook plural timestamp block getter to db

--- src/ctc/evm/block_utils/block_crud.py
+++ src/ctc/evm/block_utils/block_crud.py
@@ -89,14 +89,35 @@ async def async_get_blocks_timestamps(
     include_full_transactions: bool = False,
     chunk_size: int = 500,
     provider: spec.ProviderSpec = None,
+    use_db: bool = True,
 ) -> list[int]:
-    return [
-        block['timestamp']
-        for block in await async_get_blocks(
-            blocks=blocks,
+
+    # get timestamps from db
+    if use_db:
+        from ctc import db
+        network = rpc.get_provider_network(provider)
+        engine = db.create_engine(datatype='block_timestamps', network=network)
+        with engine.connect() as conn:
+            db_timestamps = db.get_blocks_timestamps(
+                conn=conn,
+                block_numbers=blocks,
+            )
+        results = dict(zip(blocks, db_timestamps))
+        remaining_blocks = [block for block in blocks if block is None]
+    else:
+        results = {}
+        remaining_blocks = blocks
+
+    # get timestamps from rpc
+    if len(remaining_blocks) > 0:
+        node_blocks = await async_get_blocks(
+            blocks=remaining_blocks,
             include_full_transactions=include_full_transactions,
             chunk_size=chunk_size,
             provider=provider,
         )
-    ]
+        for block_data in node_blocks:
+            results[block_data['number']] = block_data['timestamp']
+
+    return [results[block] for block in blocks]
 
