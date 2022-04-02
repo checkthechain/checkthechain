0fe7c531e82567303ff429d4a0883f0311471525
sslivkoff <stormslivkoff@gmail.com>
2022-04-02 11:44:48 -0700

    add db connect cli command

--- src/ctc/cli/cli_run.py
+++ src/ctc/cli/cli_run.py
@@ -30,6 +30,7 @@ command_index = {
     ('abi',): 'ctc.cli.commands.data.abi_command',
     ('address',): 'ctc.cli.commands.data.address_command',
     ('db', 'create', 'tables'): 'ctc.cli.commands.data.db.create_tables_command',
+    ('db', 'connect'): 'ctc.cli.commands.data.db.connect_command',
     ('block',): 'ctc.cli.commands.data.block_command',
     ('blocks',): 'ctc.cli.commands.data.blocks_command',
     ('call',): 'ctc.cli.commands.data.call_command',
---
+++ src/ctc/cli/commands/data/db/connect_command.py
@@ -0,0 +1,26 @@
+import subprocess
+
+from ctc import config
+
+
+def get_command_spec():
+    return {
+        'f': connect_command,
+        'help': 'connect to database console (proceed with caution)',
+    }
+
+
+def connect_command():
+    db_config = config.get_db_config()
+
+    if db_config['dbms'] == 'sqlite':
+        print(
+            'connecting to',
+            db_config['dbms'],
+            'database at path',
+            db_config['path'],
+        )
+        subprocess.call(['sqlite3', db_config['path']])
+    else:
+        raise Exception('unknown dbms')
+
