a8cf4d2fd4fd7ce6837e5fb32625bb8ad6ff6c81
sslivkoff <stormslivkoff@gmail.com>
2022-04-05 10:52:30 -0700

    catch reverting rari price oracle calls

--- src/ctc/protocols/rari_utils/fuse_queries/token_state/token_price.py
+++ src/ctc/protocols/rari_utils/fuse_queries/token_state/token_price.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import warnings
+
+from ctc import rpc
+from ctc import spec
 from ... import rari_abis
 from .. import pool_metadata
 from .. import token_metadata
-from ctc import rpc
 
 
 async def async_get_ctoken_exchange_rate(ctoken, block='latest'):
@@ -25,20 +28,26 @@ async def async_get_ctoken_exchange_rate_by_block(ctoken, blocks):
 
 
 async def async_get_ctoken_price(
-    ctoken, oracle=None, block='latest', normalize=True
+    ctoken, oracle=None, block='latest', normalize=True, raise_on_revert=False,
 ):
 
     if oracle is None:
         oracle = await _async_get_ctoken_oracle(ctoken=ctoken)
 
-    price = await rpc.async_eth_call(
-        to_address=oracle,
-        block_number=block,
-        function_abi=rari_abis.oracle_abis['getUnderlyingPrice'],
-        function_parameters=[ctoken],
-    )
-    if normalize:
-        price /= 1e18
+    try:
+        price = await rpc.async_eth_call(
+            to_address=oracle,
+            block_number=block,
+            function_abi=rari_abis.oracle_abis['getUnderlyingPrice'],
+            function_parameters=[ctoken],
+        )
+        if normalize:
+            price /= 1e18
+    except spec.RpcException as e:
+        if raise_on_revert:
+            raise e
+        warnings.warn('price query failed for ctoken ' + str(ctoken))
+        price = 0
 
     return price
 
