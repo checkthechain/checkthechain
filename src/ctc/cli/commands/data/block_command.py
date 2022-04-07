83dc6cab63580fa152902393680c4ae46668de5e
sslivkoff <stormslivkoff@gmail.com>
2022-04-07 15:31:26 -0700

    add json option to block output

--- src/ctc/cli/commands/data/block_command.py
+++ src/ctc/cli/commands/data/block_command.py
@@ -18,17 +18,30 @@ def get_command_spec():
                 'action': 'store_true',
                 'help': 'emit extra information',
             },
+            {
+                'name': '--json',
+                'help': 'output block as raw json',
+                'dest': 'as_json',
+                'action': 'store_true',
+            }
         ],
     }
 
 
-async def async_block_command(block, timestamp, verbose=False, **kwargs):
+async def async_block_command(block, timestamp, verbose, as_json):
     if timestamp is not None and block is None:
         print('searching for block at timestamp:', timestamp)
         block = evm.get_block_of_timestamp(timestamp=timestamp, verbose=False)
         print('found block', block)
         print()
 
-    await evm.async_print_block_summary(block=block)
+    if as_json:
+        import rich
+        import json
+        block_data = await evm.async_get_block(block)
+        rich.print_json(json.dumps(block_data))
+    else:
+        await evm.async_print_block_summary(block=block)
+
     await rpc.async_close_http_session()
 
