4aeabd89c2850254b35fcc5052464562b6472e82
sslivkoff <stormslivkoff@gmail.com>
2022-04-07 18:32:03 -0700

    add --json parameter to config subcommand

--- src/ctc/cli/commands/admin/config_command.py
+++ src/ctc/cli/commands/admin/config_command.py
@@ -13,45 +13,61 @@ def get_command_spec():
                 'action': 'store_true',
                 'help': 'show sensitive information in config',
             },
+            {
+                'name': '--json',
+                'help': 'output config as json',
+                'dest': 'as_json',
+                'action': 'store_true',
+            },
         ],
     }
 
 
-def config_command(reveal):
+def config_command(reveal, as_json):
 
     env_var = ctc.config.config_path_env_var
 
-    print('# Config Summary')
-    print('- config env variable:', env_var)
-    if env_var not in os.environ:
-        print('-', env_var, 'not set')
-    else:
-        env_value = os.environ[env_var]
-        if env_value is None or env_value == '':
-            print('-', env_var, 'set to null')
-        else:
-            print('-', env_var, 'set to:', env_value)
-    print('- config path:', ctc.config.get_config_path(raise_if_dne=False))
+    if as_json:
+        import rich
 
-    print()
-    print('## Config Values')
-    config = ctc.config.get_config()
-    for key in sorted(config.keys()):
-        if isinstance(config[key], dict) and len(config[key]) > 0:
-            print('-', str(key) + ':')
-            for subkey, subvalue in config[key].items():
-                if isinstance(subvalue, dict) and len(subvalue) > 0:
-                    print('    -', str(subkey) + ':')
-                    for subsubkey, subsubvalue in subvalue.items():
-                        if (
-                            (not reveal)
-                            and key == 'providers'
-                            and subsubkey == 'url'
-                        ):
-                            subsubvalue = '********'
-                        print('        -', str(subsubkey) + ':', subsubvalue)
-                else:
-                    print('    -', str(subkey) + ':', subvalue)
+        config = ctc.config.get_config()
+        rich.print(config)
+
+    else:
+
+        print('# Config Summary')
+        print('- config env variable:', env_var)
+        if env_var not in os.environ:
+            print('-', env_var, 'not set')
         else:
-            print('-', str(key) + ':', config[key])
+            env_value = os.environ[env_var]
+            if env_value is None or env_value == '':
+                print('-', env_var, 'set to null')
+            else:
+                print('-', env_var, 'set to:', env_value)
+        print('- config path:', ctc.config.get_config_path(raise_if_dne=False))
+
+        print()
+        print('## Config Values')
+        config = ctc.config.get_config()
+        for key in sorted(config.keys()):
+            if isinstance(config[key], dict) and len(config[key]) > 0:
+                print('-', str(key) + ':')
+                for subkey, subvalue in config[key].items():
+                    if isinstance(subvalue, dict) and len(subvalue) > 0:
+                        print('    -', str(subkey) + ':')
+                        for subsubkey, subsubvalue in subvalue.items():
+                            if (
+                                (not reveal)
+                                and key == 'providers'
+                                and subsubkey == 'url'
+                            ):
+                                subsubvalue = '********'
+                            print(
+                                '        -', str(subsubkey) + ':', subsubvalue
+                            )
+                    else:
+                        print('    -', str(subkey) + ':', subvalue)
+            else:
+                print('-', str(key) + ':', config[key])
 
