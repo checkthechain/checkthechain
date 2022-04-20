d5e17c8dbe8a4880798958c9132e838e34b7a108
sslivkoff <stormslivkoff@gmail.com>
2022-04-20 12:27:18 -0700

    test that files import future annotations

---
+++ tests/ctc/test_code.py
@@ -0,0 +1,32 @@
+import os
+
+import ctc
+
+
+def test_code_imports_future_annotations():
+
+    phrase = 'from __future__ import annotations'
+    root_path = ctc.__path__[0]
+
+    missing = []
+    for root, dirnames, filenames in os.walk(root_path):
+        for filename in filenames:
+            if not filename.startswith('__') and filename.endswith('.py'):
+                filepath = os.path.join(root, filename)
+                with open(filepath, 'r') as f:
+                    content = f.read()
+                if phrase not in content:
+                    missing.append(filepath)
+
+    if len(missing) > 0:
+        relpaths = [
+            os.path.relpath(path, root_path)
+            for path in missing
+        ]
+        print()
+        print()
+        print(len(missing), 'paths with missing future annotation import:')
+        print()
+        print(' '.join(relpaths))
+    assert len(missing) == 0
+
