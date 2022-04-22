b2abd31a7c1e14bb70938839b6aafa4961e3d9f3
sslivkoff <stormslivkoff@gmail.com>
2022-04-21 17:32:00 -0700

    add function for probing fei dex liquidity depth

---
+++ src/ctc/protocols/fei_utils/depth/fei_uniswap_depth.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+import asyncio
+
+from ctc import directory
+from ctc.protocols import uniswap_v3_utils
+
+
+async def async_get_fei_uniswap_pools():
+    # TODO: automate
+
+    FEI = directory.get_erc20_address('FEI')
+    DAI = directory.get_erc20_address('DAI')
+    USDC = directory.get_erc20_address('USDC')
+
+    # token_in, token_out, fee
+    pools = [
+        [FEI, DAI, 500, 18, 18],
+        [FEI, USDC, 100, 18, 6],
+        [FEI, USDC, 500, 18, 6],
+    ]
+
+    return pools
+
+
+async def async_get_fei_uniswap_pool_price_depth(
+    pools=None, prices=None
+):
+    if pools is None:
+        pools = await async_get_fei_uniswap_pools()
+
+    if prices is None:
+        prices = [
+            0.995,
+            0.993,
+            0.990,
+        ]
+
+    coroutines = {}
+    for price_level in prices:
+        for p in range(len(pools)):
+            (
+                token_in,
+                token_out,
+                fee,
+                token_in_decimals,
+                token_out_decimals,
+            ) = pools[p]
+            swap_kwargs = {
+                'token_in': token_in,
+                'token_out': token_out,
+                'fee': fee,
+                'token_in_decimals': token_in_decimals,
+                'token_out_decimals': token_out_decimals,
+            }
+
+            coroutines[
+                (p, price_level)
+            ] = uniswap_v3_utils.async_get_liquidity_depth(
+                new_price=price_level,
+                **swap_kwargs,
+            )
+
+    depths = await asyncio.gather(*coroutines.values())
+
+    pool_price_depth = {}
+    for (p, price_level), depth in zip(coroutines.keys(), depths):
+        token_in, token_out, fee, token_in_decimals, token_out_decimals = pools[
+            p
+        ]
+
+        symbol_in = directory.get_erc20_metadata(address=token_in)['symbol']
+        symbol_out = directory.get_erc20_metadata(address=token_out)['symbol']
+        pool = (
+            'Uniswap V3 '
+            + symbol_in
+            + '_'
+            + symbol_out
+            + ' '
+            + ('%.2d' % (fee / 100))
+        )
+
+        pool_price_depth.setdefault(pool, {})
+        pool_price_depth[pool][price_level] = depth
+
+    return pool_price_depth
+
