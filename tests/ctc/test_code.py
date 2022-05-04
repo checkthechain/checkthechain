780b2a677f5f275cb856934bac7f4e672b9a3290
sslivkoff <stormslivkoff@gmail.com>
2022-05-04 13:04:38 -0700

    add __init__.py to all modules and enforce using testing

--- tests/ctc/test_code.py
+++ tests/ctc/test_code.py
@@ -30,3 +30,27 @@ def test_code_imports_future_annotations():
         print(' '.join(relpaths))
     assert len(missing) == 0
 
+
+def test_every_module_has_init_file():
+    root_path = ctc.__path__[0]
+
+    missing = []
+    for root, dirnames, filenames in os.walk(root_path):
+        needs_init = any(filename.endswith('.py') for filename in filenames)
+
+        if needs_init:
+            if '__init__.py' not in os.listdir(root):
+                missing.append(root)
+
+    if len(missing) > 0:
+        relpaths = [
+            os.path.relpath(path, root_path)
+            for path in missing
+        ]
+        print()
+        print()
+        print(len(missing), 'modules with missing __init__:')
+        print()
+        print(' '.join(relpaths))
+    assert len(missing) == 0
+
