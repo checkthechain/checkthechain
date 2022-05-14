9a0395881d6cbb0059aeeae6c3628c8571b60300
sslivkoff <stormslivkoff@gmail.com>
2022-05-14 14:01:13 -0700

    use proper typing on create tables cli command

--- src/ctc/cli/commands/data/db/create_tables_command.py
+++ src/ctc/cli/commands/data/db/create_tables_command.py
@@ -31,8 +31,10 @@ async def async_create_tables_command(
     datatypes: typing.Sequence[str],
     networks: typing.Sequence[str],
 ) -> None:
+    for datatype in datatypes:
+        if datatype not in db.DBDatatype.__args__:  # type: ignore
+            raise Exception('unknown datatype: ' + str(datatype))
     db.create_tables(
-        datatypes=datatypes,
+        datatypes=typing.cast(typing.Sequence[db.DBDatatype], datatypes),
         networks=networks,
     )
-
