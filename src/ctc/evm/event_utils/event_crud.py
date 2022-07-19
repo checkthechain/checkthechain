9f74cfd8cbf5e80821b5d11f3060ea0c4229a131
Rafael Ugolini <rafael.ugolini@gmail.com>
2022-07-19 21:16:23 +0200

    feat: add helper for event timestamps

--- src/ctc/evm/event_utils/event_crud.py
+++ src/ctc/evm/event_utils/event_crud.py
@@ -219,3 +219,16 @@ async def async_download_events(
         verbose=verbose,
         provider=provider,
     )
+
+
+async def async_get_event_timestamps(events: spec.DataFrame) -> spec.DataFrame:
+
+    # get block_numbers
+    multi_index = 'block_number' in events.index.names
+    if multi_index:
+        block_numbers = events.index.get_level_values('block_number')
+    else:
+        block_numbers = events.index.values
+
+    # get timestamps
+    return await block_utils.async_get_block_timestamps(block_numbers)
