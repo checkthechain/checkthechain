00dae42efdad667d19f628d13ff736462fd3ba2c
Rafael Ugolini <rafael.ugolini@gmail.com>
2022-07-21 11:45:40 +0200

    feat: include timestamps in the command line

--- src/ctc/cli/commands/data/erc20/transfers_command.py
+++ src/ctc/cli/commands/data/erc20/transfers_command.py
@@ -20,6 +20,12 @@ def get_command_spec() -> toolcli.CommandSpec:
                 'help': 'block range of transfers',
             },
             {
+                'name': '--timestamps',
+                'default': False,
+                'action': 'store_true',
+                'help': 'include timestamps',
+            },
+            {
                 'name': '--output',
                 'default': 'stdout',
                 'help': 'file path for output (.json or .csv)',
@@ -33,6 +39,7 @@ def get_command_spec() -> toolcli.CommandSpec:
         'examples': {
             '0x956f47f50a910163d8bf957cf5846d573e7f87ca': {'long': True},
             '0x956f47f50a910163d8bf957cf5846d573e7f87ca --blocks [14000000, 14001000]': {},
+            '0x956f47f50a910163d8bf957cf5846d573e7f87ca --blocks [14000000, 14001000] --timestamps': {},
         },
     }
 
@@ -43,6 +50,7 @@ async def async_transfers_command(
     blocks: typing.Sequence[str],
     output: str,
     overwrite: bool,
+    timestamps: bool,
 ) -> None:
 
     if blocks is not None:
@@ -57,5 +65,7 @@ async def async_transfers_command(
         erc20,
         start_block=start_block,
         end_block=end_block,
+        include_timestamps=timestamps,
     )
+    transfers = transfers.astype({'timestamp': 'str'})
     cli_utils.output_data(transfers, output=output, overwrite=overwrite)
