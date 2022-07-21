22651cff0469cfc507801eae9f5196677d42b3ee
Rafael Ugolini <rafael.ugolini@gmail.com>
2022-07-21 23:34:40 +0200

    docs: update example

--- src/ctc/cli/commands/data/events_command.py
+++ src/ctc/cli/commands/data/events_command.py
@@ -54,7 +54,7 @@ def get_command_spec() -> toolcli.CommandSpec:
                 'long': True
             },
             '0x956f47f50a910163d8bf957cf5846d573e7f87ca Transfer --blocks [14000000, 14010000]': {},
-            '0x956f47f50a910163d8bf957cf5846d573e7f87ca Transfer --blocks [14000000, 14010000] --timestamps': {},
+            '0x956f47f50a910163d8bf957cf5846d573e7f87ca Transfer --blocks [14000000, 14010000] --include-timestamps': {},
         },
     }
 
