d101134f7c525e93fc08cfd59b344e470fe2e5cc
sslivkoff <stormslivkoff@gmail.com>
2022-07-20 20:40:46 -0700

    remove extraneous tuple version getter from __init__

--- src/ctc/__init__.py
+++ src/ctc/__init__.py
@@ -1,19 +1,3 @@
 """ctc is a tool for collecting and processing historical EVM data"""
 
-from __future__ import annotations
-
-
 __version__ = '0.3.0'
-
-
-def get_version_tuple() -> tuple[int, int, int]:
-
-    # remove alpha, beta, and release candidate tokens
-    version_str = __version__
-    for token in ['a', 'b', 'rc']:
-        if token in version_str:
-            version_str = version_str[version_str.index(token)]
-            break
-
-    tokens = version_str.split('.')
-    return (int(tokens[0]), int(tokens[1]), int(tokens[2]))
