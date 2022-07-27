f0a4b3f639b95c0e78365a5fb3087b7ae1cfd975
sslivkoff <stormslivkoff@gmail.com>
2022-07-27 01:31:43 -0700

    add many options to the abi cli command

--- src/ctc/cli/commands/data/abi_command.py
+++ src/ctc/cli/commands/data/abi_command.py
@@ -17,7 +17,11 @@ def get_command_spec() -> toolcli.CommandSpec:
         'help': 'display abi of contract',
         'args': [
             {'name': 'address', 'help': 'address of contract'},
-            {'name': 'name', 'help': 'name of function or event', 'nargs': '?'},
+            {
+                'name': 'names',
+                'help': 'name of function or event',
+                'nargs': '*',
+            },
             {
                 'name': '--json',
                 'dest': 'json_pretty',
@@ -30,6 +34,16 @@ def get_command_spec() -> toolcli.CommandSpec:
                 'action': 'store_true',
             },
             {
+                'name': '--map-names',
+                'help': 'output ABI as map where names are the keys',
+                'action': 'store_true',
+            },
+            {
+                'name': '--map-selectors',
+                'help': 'output ABI as map where hash selectors are the keys',
+                'action': 'store_true',
+            },
+            {
                 'name': '--functions',
                 'help': 'display function abi\'s only',
                 'action': 'store_true',
@@ -39,7 +53,6 @@ def get_command_spec() -> toolcli.CommandSpec:
                 'help': 'display event abi\'s only',
                 'action': 'store_true',
             },
-            {'name': '--name', 'help': 'name of function or event abi'},
             {
                 'name': '--search',
                 'help': 'query of name of function or event abi',
@@ -49,13 +62,17 @@ def get_command_spec() -> toolcli.CommandSpec:
                 'help': 'include extra abi data',
                 'action': 'store_true',
             },
+            {
+                'name': '--python',
+                'help': 'output data as pep8-formatted python syntax',
+                'action': 'store_true',
+            },
         ],
         'examples': [
             '0x956f47f50a910163d8bf957cf5846d573e7f87ca',
+            '0x956f47f50a910163d8bf957cf5846d573e7f87ca Transfer Minting',
             '0x956f47f50a910163d8bf957cf5846d573e7f87ca --json',
-            '0x956f47f50a910163d8bf957cf5846d573e7f87ca --functions',
-            '0x956f47f50a910163d8bf957cf5846d573e7f87ca --functions --json',
-            '0x956f47f50a910163d8bf957cf5846d573e7f87ca --name Transfer',
+            '0x956f47f50a910163d8bf957cf5846d573e7f87ca --map-names --python',
         ],
     }
 
@@ -63,24 +80,34 @@ def get_command_spec() -> toolcli.CommandSpec:
 async def async_abi_command(
     *,
     address: spec.Address,
-    name: str | None,
+    names: typing.Sequence[str],
     json_pretty: bool,
     json_raw: bool,
     functions: bool,
     events: bool,
     search: str,
     verbose: bool,
+    map_names: bool,
+    map_selectors: bool,
+    python: bool,
 ) -> None:
+
+    if map_names and map_selectors:
+        raise Exception('can only specify one of --map-names or --map-keys')
+    if map_names or map_selectors:
+        if not json_raw:
+            json_pretty = True
 
     address = await evm.async_resolve_address(address)
     contract_abi = await evm.async_get_contract_abi(contract_address=address)
 
     # filter by name
-    if name is not None:
+    if len(names) > 0:
         contract_abi = [
             item
             for item in contract_abi
-            if item.get('name') is not None and name == item['name']
+            if item.get('name') is not None
+            and any(name == item['name'] for name in names)
         ]
     if search is not None:
         search = search.lower()
@@ -106,10 +133,31 @@ async def async_abi_command(
     if not human_only:
         import json
 
-        if json_raw:
-            as_str = json.dumps(contract_abi)
+        if map_names:
+            json_data: typing.Any = {}
+            for item in contract_abi:
+                name = item.get('name')
+                if name is None:
+                    continue
+                if name in json_data:
+                    raise Exception('ABI naming conflict: ' + str(name))
+                json_data[name] = item
+        elif map_selectors:
+            json_data = evm.get_contract_abi_by_selectors(contract_abi)
         else:
-            as_str = json.dumps(contract_abi, indent=4, sort_keys=True)
+            json_data = contract_abi
+
+        if json_raw:
+            as_str = json.dumps(json_data)
+        else:
+            as_str = json.dumps(json_data, indent=4, sort_keys=True)
+
+        if python:
+            import re
+
+            as_str = re.sub('(?<![\[{,])\n', ',\n', as_str)
+            as_str = as_str.replace(': true,\n', ': True,\n')
+            as_str = as_str.replace(': false,\n', ': False,\n')
         print(as_str)
     if not json_only and not human_only:
         print()
