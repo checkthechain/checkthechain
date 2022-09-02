6da433aea825e953103863cb8554242a09cf6154
sslivkoff <stormslivkoff@gmail.com>
2022-09-02 13:50:20 -0700

    add --json option to tx command

--- src/ctc/cli/commands/data/tx_command.py
+++ src/ctc/cli/commands/data/tx_command.py
@@ -12,6 +12,12 @@ def get_command_spec() -> toolcli.CommandSpec:
         'args': [
             {'name': 'transaction', 'help': 'hash of transaction'},
             {'name': '--sort', 'help': 'attribute to sort logs by'},
+            {
+                'name': '--json',
+                'help': 'output raw JSON of tx',
+                'action': 'store_true',
+                'dest': 'as_json',
+            },
         ],
         'examples': [
             '0xe981fe5c78d11d935a1dc35c579969e65e2dd6bb05ad321ea9670f8b1e203eaf',
@@ -19,8 +25,17 @@ def get_command_spec() -> toolcli.CommandSpec:
     }
 
 
-async def async_transaction_command(transaction: str, sort: str) -> None:
+async def async_transaction_command(
+    *, transaction: str, sort: str, as_json: bool
+) -> None:
 
-    await evm.async_print_transaction_summary(
-        transaction_hash=transaction, sort_logs_by=sort
-    )
+    if as_json:
+        import json
+
+        transaction_data = await evm.async_get_transaction(transaction)
+        print(json.dumps(transaction_data, sort_keys=True, indent=4))
+
+    else:
+        await evm.async_print_transaction_summary(
+            transaction_hash=transaction, sort_logs_by=sort
+        )
