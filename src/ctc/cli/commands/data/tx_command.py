c672427d72e5a2f5ea3899e71af4eff782c4dd4d
Jamie Rumbelow <jamie@jamierumbelow.net>
2022-10-01 21:49:32 +0100

    add receipt flag to ctc tx

--- src/ctc/cli/commands/data/tx_command.py
+++ src/ctc/cli/commands/data/tx_command.py
@@ -4,7 +4,6 @@ import toolcli
 
 from ctc import evm
 
-
 def get_command_spec() -> toolcli.CommandSpec:
     return {
         'f': async_transaction_command,
@@ -18,24 +17,48 @@ def get_command_spec() -> toolcli.CommandSpec:
                 'action': 'store_true',
                 'dest': 'as_json',
             },
+            {
+                'name': '--receipt',
+                'help': 'output transaction receipt (only works with --json)',
+                'action': 'store_true',
+                'dest': 'receipt',
+            },
         ],
         'examples': [
             '0xe981fe5c78d11d935a1dc35c579969e65e2dd6bb05ad321ea9670f8b1e203eaf',
+            '0xe981fe5c78d11d935a1dc35c579969e65e2dd6bb05ad321ea9670f8b1e203eaf --json',
+            '0xe981fe5c78d11d935a1dc35c579969e65e2dd6bb05ad321ea9670f8b1e203eaf --json --receipt',
         ],
     }
 
 
 async def async_transaction_command(
-    *, transaction: str, sort: str, as_json: bool
+    *, transaction: str, sort: str, as_json: bool, receipt: bool
 ) -> None:
 
     if as_json:
         import json
+        import asyncio
+        from ctc import rpc
+
+        if receipt:
+            receipt_task = asyncio.create_task(
+                rpc.async_eth_get_transaction_receipt(
+                    transaction_hash=transaction
+                )
+            )
+            receipt_data = await receipt_task
+            print(json.dumps(receipt_data, sort_keys=True, indent=4))
+            return
 
         transaction_data = await evm.async_get_transaction(transaction)
         print(json.dumps(transaction_data, sort_keys=True, indent=4))
 
     else:
+        if receipt:
+            print('receipt flag only works with --json')
+            return
+        
         await evm.async_print_transaction_summary(
             transaction_hash=transaction, sort_logs_by=sort
         )
