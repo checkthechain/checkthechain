6ac5fc6a55e80a83e12844dc09da9f4cc756ea78
sslivkoff <stormslivkoff@gmail.com>
2022-12-31 18:11:43 -0800

    improve human readble config printout

--- src/ctc/cli/commands/admin/config_command.py
+++ src/ctc/cli/commands/admin/config_command.py
@@ -63,6 +63,7 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
             title_style = None
             chrome_style = None
             content_style = None
+            description_style = None
             path_style = None
             key_style = None
 
@@ -71,7 +72,8 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
             title_style = styles['title']
             chrome_style = styles['comment']
             key_style = styles['option']
-            content_style = styles['description']
+            content_style = styles['content']
+            description_style = styles['description']
             path_style = styles['metavar']
 
             path_style = path_style + ' bold'
@@ -133,15 +135,15 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
                         network_metadata['block_explorer'],
                     ]
                     rows.append(row)
-                labels = ['name', 'chain_id', 'block_explorer']
+                labels = ['name', 'chain id', 'block_explorer']
                 toolstr.print_table(
                     rows,
                     labels=labels,
                     indent=4,
                     label_style=title_style,
                     column_styles={
-                        'name': content_style,
-                        'chain_id': content_style,
+                        'name': description_style,
+                        'chain id': content_style,
                         'block_explorer': path_style,
                     },
                     border=chrome_style,
@@ -156,7 +158,7 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
                 )
                 if len(config[key]) == 0:
                     continue
-                labels = ['name', 'network', 'url']
+                labels = ['name', 'network', 'chain id', 'url']
                 rows = []
                 for provider in config[key].values():
                     row = []
@@ -167,6 +169,12 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
                             cell = config['networks'][provider['network']][
                                 'name'
                             ]
+                        elif label == 'chain id':
+                            cell = config['networks'][provider['network']][
+                                'chain_id'
+                            ]
+                        elif label == 'name':
+                            cell = provider['name']
                         else:
                             cell = str(provider[label])
                         row.append(cell)
@@ -176,12 +184,17 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
                     labels=labels,
                     indent=4,
                     label_style=title_style,
-                    column_styles={
-                        'name': content_style,
-                        'network': content_style,
-                        'url': path_style,
-                    },
                     border=chrome_style,
+                    column_styles={
+                        'name': description_style,
+                        'network': description_style,
+                        'url': path_style,
+                        'chain id': content_style,
+                    },
+                    column_formats={
+                        'chain id': {'commas': False},
+                    },
+                    sort_column=['chain id', 'name'],
                 )
 
                 if not reveal:
@@ -205,7 +218,7 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
                     if isinstance(subvalue, dict) and len(subvalue) > 0:
                         toolstr.print(
                             toolstr.add_style('    -', chrome_style),
-                            toolstr.add_style(str(subkey), content_style)
+                            toolstr.add_style(str(subkey), description_style)
                             + toolstr.add_style(':', chrome_style),
                         )
                         for subsubkey, subsubvalue in subvalue.items():
@@ -213,27 +226,72 @@ def config_command(*, reveal: bool, as_json: bool, no_color: bool) -> None:
                             if key == 'db_configs' and subsubkey == 'path':
                                 style = path_style
                             else:
-                                style = content_style
+                                style = description_style
 
                             toolstr.print(
                                 toolstr.add_style('        -', chrome_style),
-                                toolstr.add_style(str(subsubkey), content_style)
+                                toolstr.add_style(
+                                    str(subsubkey), description_style
+                                )
                                 + toolstr.add_style(':', chrome_style),
                                 toolstr.add_style(subsubvalue, style),
                             )
                     else:
                         toolstr.print(
                             toolstr.add_style('    -', chrome_style),
-                            toolstr.add_style(str(subkey), content_style)
+                            toolstr.add_style(str(subkey), description_style)
                             + toolstr.add_style(':', chrome_style),
-                            toolstr.add_style(str(subvalue), content_style),
+                            toolstr.add_style(str(subvalue), description_style),
                         )
+
+            elif key == 'context_cache_rules':
+
+                rules = config[key]
+                rows = []
+                for rule in rules:
+                    row = []
+                    row.append(rule.get('backend', ''))
+                    row.append(rule.get('read', ''))
+                    row.append(rule.get('write', ''))
+                    filter = rule.get('filter')
+                    if filter is None or len(filter) == 0:
+                        scope = 'global'
+                    else:
+                        scope = ','.join(
+                            (key + '=' + value) for key, value in filter.items()
+                        )
+                    row.append(scope)
+                    rows.append(row)
+
+                toolstr.print_bullet(
+                    key='cache configuration', value='', key_style=key_style
+                )
+                labels = ['backend', 'read', 'write', 'scope']
+                toolstr.print_table(
+                    rows,
+                    labels=labels,
+                    indent=8,
+                    label_style=title_style,
+                    border=chrome_style,
+                    column_styles={
+                        'backend': path_style,
+                        'read': description_style,
+                        'write': description_style,
+                        'scope': content_style,
+                    },
+                    add_row_index=True,
+                )
+                toolstr.print(
+                    '(rules applied in order)',
+                    style=chrome_style,
+                    indent=8,
+                )
 
             else:
                 if key == 'data_dir':
                     style = path_style
                 else:
-                    style = content_style
+                    style = description_style
 
                 toolstr.print(
                     toolstr.add_style('-', chrome_style),
