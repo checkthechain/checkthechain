cacbb1525231c1daa4b3313ea2cf4d67ee93f6e4
sslivkoff <stormslivkoff@gmail.com>
2023-01-09 00:56:23 -0800

    allow multiple schema names to create_engine

--- src/ctc/db/connect_utils.py
+++ src/ctc/db/connect_utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import typing
 
 import toolsql
 
@@ -11,12 +12,29 @@ from .management import version_utils
 
 
 def create_engine(
-    schema_name: spec.SchemaName,
+    schema_name: spec.SchemaName | None = None,
     *,
+    schema_names: typing.Sequence[spec.SchemaName] | None = None,
     context: spec.Context = None,
     create_missing_schema: bool = True,
 ) -> toolsql.SAEngine | None:
-    """create sqlalchemy engine object"""
+    """create sqlalchemy engine object
+
+    TODO: modify behavior of `schema_names` parameter once multicaches supported
+    """
+
+    if schema_name is not None and schema_names is not None:
+        raise Exception('specify only one of schema_name or schema_names')
+    if schema_name is None:
+        if schema_names is None or len(schema_names) == 0:
+            raise Exception('must specify schema_name or schema_names')
+        else:
+            schema_name = schema_names[0]
+    if schema_names is None:
+        if schema_name is None:
+            raise Exception('must specify schema_name or schema_names')
+        else:
+            schema_names = [schema_name]
 
     network = config.get_context_chain_id(context)
 
@@ -41,19 +59,21 @@ def create_engine(
             if not version_utils.is_schema_versions_initialized(engine=engine):
                 dba_utils.initialize_schema_versions(conn=conn)
 
-            # check if schema in database
-            schema_version = version_utils.get_schema_version(
-                schema_name=schema_name,
-                context=dict(network=network),
-            )
+            for schema_name in schema_names:
 
-            # create schema if missing
-            if schema_version is None:
-                dba_utils.initialize_schema(
+                # check if schema in database
+                schema_version = version_utils.get_schema_version(
                     schema_name=schema_name,
-                    context=context,
-                    conn=conn,
+                    context=dict(network=network),
                 )
+
+                # create schema if missing
+                if schema_version is None:
+                    dba_utils.initialize_schema(
+                        schema_name=schema_name,
+                        context=context,
+                        conn=conn,
+                    )
 
     return engine
 
