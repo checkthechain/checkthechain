199b656f2b3a7dfbc9bef581d9f5fa0af4e6a007
sslivkoff <stormslivkoff@gmail.com>
2023-01-13 22:09:27 -0800

    add indices to more transaction columns

--- src/ctc/db/schemas/transactions/transactions_schema_defs.py
+++ src/ctc/db/schemas/transactions/transactions_schema_defs.py
@@ -8,10 +8,10 @@ transactions_schema: toolsql.DBSchema = {
         'transactions': {
             'columns': [
                 {'name': 'hash', 'type': 'Text', 'primary': True},
-                {'name': 'block_number', 'type': 'Integer'},
+                {'name': 'block_number', 'type': 'Integer', 'index': True},
                 {'name': 'transaction_index', 'type': 'Integer'},
-                {'name': 'to_address', 'type': 'Text'},
-                {'name': 'from_address', 'type': 'Text'},
+                {'name': 'to_address', 'type': 'Text', 'index': True},
+                {'name': 'from_address', 'type': 'Text', 'index': True},
                 {'name': 'value', 'type': 'Text'},  # int <-> str
                 {'name': 'input', 'type': 'Text'},
                 {'name': 'nonce', 'type': 'Integer'},
