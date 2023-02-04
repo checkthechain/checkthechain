c937b09cc13276ed2a96861cf9d76fec1ca00053
sslivkoff <stormslivkoff@gmail.com>
2023-02-03 19:06:00 -0800

    add initial pl_utils

---
+++ src/ctc/toolbox/pl_utils/summary_utils.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+import polars as pl
+
+
+def set_column_display_width(width: int = 70):
+    pl.Config.set_fmt_str_lengths(width)
+
+
+# def value_counts(df: pl.DataFrame, column: str) -> pl.Series:
+#     return df.select(pl.col(column).value_counts(sort=True)).unnest(column)
+
