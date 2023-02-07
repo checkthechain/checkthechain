d07730fc5e14abbbeaceba521cc0115e3a347421
sslivkoff <stormslivkoff@gmail.com>
2023-02-07 11:49:40 -0800

    add debug_* trace support

--- src/ctc/rpc/rpc_executors/rpc_trace_executors.py
+++ src/ctc/rpc/rpc_executors/rpc_trace_executors.py
@@ -191,3 +191,114 @@ async def async_trace_replay_block_transactions(
         response=response
     )
 
+
+#
+# # debug traces
+#
+
+
+async def async_debug_trace_call(
+    to_address: spec.BinaryData,
+    *,
+    from_address: spec.BinaryData | None = None,
+    gas: spec.BinaryData | None = None,
+    gas_price: spec.BinaryData | None = None,
+    value_sent: spec.BinaryData | None = None,
+    call_data: spec.BinaryData | None = None,
+    function_parameters: typing.Sequence[typing.Any]
+    | typing.Mapping[str, typing.Any]
+    | None = None,
+    function_abi: spec.FunctionABI | None = None,
+    trace_type: spec.TraceOutputType,
+    context: spec.Context = None,
+    snake_case_response: bool = True,
+    block_number: spec.BlockNumberReference | None = None,
+) -> spec.RpcSingularResponse:
+
+    request = rpc_constructors.construct_debug_trace_call(
+        to_address=to_address,
+        from_address=from_address,
+        gas=gas,
+        gas_price=gas_price,
+        value_sent=value_sent,
+        block_number=block_number,
+        call_data=call_data,
+        function_parameters=function_parameters,
+        function_abi=function_abi,
+        trace_type=trace_type,
+    )
+    response = await rpc_request.async_send(request, context=context)
+    return rpc_digestors.digest_debug_trace_call(
+        response=response,
+        snake_case_response=snake_case_response,
+    )
+
+
+async def async_debug_trace_call_many(
+    calls: typing.Sequence[typing.Mapping[str, typing.Any]],
+    trace_type: spec.TraceOutputType | None,
+    *,
+    block_number: spec.BlockNumberReference | None = None,
+    context: spec.Context = None,
+    snake_case_response: bool = True,
+) -> spec.RpcSingularResponse:
+    request = rpc_constructors.construct_debug_trace_call_many(
+        calls=calls,
+        trace_type=trace_type,
+        block_number=block_number,
+    )
+    response = await rpc_request.async_send(request, context=context)
+    return rpc_digestors.digest_debug_trace_call_many(
+        response=response,
+        snake_case_response=snake_case_response,
+    )
+
+
+async def async_debug_trace_transaction(
+    transaction_hash: str,
+    *,
+    context: spec.Context = None,
+    snake_case_response: bool = True,
+) -> spec.RpcSingularResponse:
+
+    request = rpc_constructors.construct_debug_trace_transaction(
+        transaction_hash=transaction_hash
+    )
+    response = await rpc_request.async_send(request, context=context)
+    return rpc_digestors.digest_debug_trace_transaction(
+        response=response,
+        snake_case_response=snake_case_response,
+    )
+
+
+async def async_debug_trace_block_by_number(
+    block_number: spec.BlockNumberReference,
+    *,
+    context: spec.Context = None,
+    snake_case_response: bool = True,
+) -> spec.RpcSingularResponse:
+    request = rpc_constructors.construct_debug_trace_block_by_number(
+        block_number=block_number,
+    )
+    response = await rpc_request.async_send(request, context=context)
+    return rpc_digestors.digest_debug_trace_block_by_number(
+        response=response,
+        snake_case_response=snake_case_response,
+    )
+
+
+async def async_debug_trace_block_by_hash(
+    block_hash: str,
+    *,
+    context: spec.Context = None,
+    snake_case_response: bool = True,
+) -> spec.RpcSingularResponse:
+    request = rpc_constructors.construct_debug_trace_block_by_hash(
+        block_hash=block_hash,
+    )
+    response = await rpc_request.async_send(request, context=context)
+    return rpc_digestors.digest_debug_trace_block_by_hash(
+        response=response,
+        snake_case_response=snake_case_response,
+    )
+
