1c97f4af465991e7ebb8683144872375b3ed3ee6
sslivkoff <stormslivkoff@gmail.com>
2023-04-21 09:41:30 -0700

    add format agnostic dataframe writer

--- src/ctc/toolbox/pl_utils/file_utils.py
+++ src/ctc/toolbox/pl_utils/file_utils.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+import os
 import typing
 
-import os
+import polars as pl
 
 
 def create_multipath_glob(paths: typing.Sequence[str]) -> str:
@@ -18,6 +19,32 @@ def create_multipath_glob(paths: typing.Sequence[str]) -> str:
     return os.path.join(temp_dir, '*')
 
 
+def write_df(
+    df: pl.DataFrame,
+    path: str,
+    *,
+    n_row_groups: int | None = None,
+    row_group_size: int | None = None,
+    **write_kwargs: typing.Any,
+) -> None:
+    import shutil
+
+    if n_row_groups is not None:
+        write_kwargs['row_group_size'] = int(len(df) / n_row_groups)
+    elif row_group_size is not None:
+        write_kwargs['row_group_size'] = row_group_size
+
+    temp_path = path + '_temp'
+    if path.endswith('.parquet'):
+        df.write_parquet(temp_path, **write_kwargs)
+    elif path.endswith('.csv'):
+        df.write_csv(temp_path, **write_kwargs)
+    else:
+        raise Exception('unknown file format: ' + str(path))
+
+    shutil.move(temp_path, path)
+
+
 # def add_stats_to_parquet_file(
 #     path: str,
 #     *,
@@ -29,9 +56,10 @@ def create_multipath_glob(paths: typing.Sequence[str]) -> str:
 #             new_path = path + '_tmp'
 #         else:
 #             pass
-    
+
 #     df = pl.read_parquet(path)
 #     df.write_parquet(new_path, statistics=True)
-    
+
 #     if replace:
 #         shutil.move(new_path, path)
+
