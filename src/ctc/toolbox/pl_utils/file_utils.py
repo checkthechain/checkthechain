cfbcba026312999d9ab7d87887528dd55b826ea2
sslivkoff <stormslivkoff@gmail.com>
2023-04-23 23:26:49 -0700

    add many options to pl_utils.write_df()

--- src/ctc/toolbox/pl_utils/file_utils.py
+++ src/ctc/toolbox/pl_utils/file_utils.py
@@ -20,25 +20,50 @@ def create_multipath_glob(paths: typing.Sequence[str]) -> str:
 
 
 def write_df(
-    df: pl.DataFrame,
+    df: pl.DataFrame | pl.LazyFrame,
     path: str,
     *,
     n_row_groups: int | None = None,
     row_group_size: int | None = None,
+    statistics: bool = True,
+    create_dir: bool = False,
+    overwrite: bool = False,
     **write_kwargs: typing.Any,
 ) -> None:
     import shutil
 
+    if create_dir:
+        dirpath = os.path.dirname(path)
+        if len(dirpath) > 0:
+            os.makedirs(dirpath, exist_ok=True)
+
+    if os.path.exists(path) and not overwrite:
+        raise Exception('file already exists, use overwrite=True: ' + str(path))
+
     if n_row_groups is not None:
+        if isinstance(df, pl.LazyFrame):
+            raise Exception('cannot specify n_row_groups with LazyFrame\'s')
         write_kwargs['row_group_size'] = int(len(df) / n_row_groups)
+        if write_kwargs['row_group_size'] == 0:
+            write_kwargs['row_group_size'] = 1
     elif row_group_size is not None:
         write_kwargs['row_group_size'] = row_group_size
 
     temp_path = path + '_temp'
     if path.endswith('.parquet'):
-        df.write_parquet(temp_path, **write_kwargs)
+        if isinstance(df, pl.LazyFrame):
+            df.sink_parquet(temp_path, statistics=statistics, **write_kwargs)
+        elif isinstance(df, pl.DataFrame):
+            df.write_parquet(temp_path, statistics=statistics, **write_kwargs)
+        else:
+            raise Exception('unknown dataframe type')
     elif path.endswith('.csv'):
-        df.write_csv(temp_path, **write_kwargs)
+        if isinstance(df, pl.LazyFrame):
+            df.collect(streaming=True).write_csv(temp_path, **write_kwargs)
+        elif isinstance(df, pl.DataFrame):
+            df.write_csv(temp_path, **write_kwargs)
+        else:
+            raise Exception('unknown dataframe type')
     else:
         raise Exception('unknown file format: ' + str(path))
 
