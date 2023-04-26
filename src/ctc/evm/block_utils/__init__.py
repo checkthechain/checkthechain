1e9e8cb532c47d74fb6f19c4b390f24f62c6cf9f
sslivkoff <stormslivkoff@gmail.com>
2023-04-25 21:39:30 -0700

    import block_samples in block_utils automatically

--- src/ctc/evm/block_utils/__init__.py
+++ src/ctc/evm/block_utils/__init__.py
@@ -4,5 +4,6 @@ from .block_crud import *
 from .block_gas import *
 from .block_hashes import *
 from .block_normalize import *
+from .block_samples import *
 from .block_summary import *
 from .block_times import *
