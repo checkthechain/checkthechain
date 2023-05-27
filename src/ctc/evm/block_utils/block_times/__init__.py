7cee2d524cd4cb28a544f353530a1cfcdb1fec6e
sslivkoff <stormslivkoff@gmail.com>
2023-05-27 12:08:53 -0700

    refactor block_time_columns as block_time_bins

--- src/ctc/evm/block_utils/block_times/__init__.py
+++ src/ctc/evm/block_utils/block_times/__init__.py
@@ -1,4 +1,4 @@
 from .timestamp_to_block import *
-from .block_time_columns import *
+from .block_time_bins import *
 from .block_time_predictions import *
 from .block_to_timestamp import *
--- src/ctc/evm/block_utils/block_times/block_time_bins.py
+++ src/ctc/evm/block_utils/block_times/block_time_bins.py
@@ -9,104 +9,106 @@ if typing.TYPE_CHECKING:
     import tooltime
 
 
-async def async_compute_column_block_month(
-    block_number_series: spec.Series,
+async def async_compute_block_months(
+    block_numbers: typing.Sequence[int] | spec.Series,
 ) -> spec.Series:
-    return await async_compute_column_block_interval(
-        block_number_series,
+    return await async_compute_block_time_bins(
+        block_numbers,
         interval_size='1M',
     )
 
 
-async def async_compute_column_block_year(
-    block_number_series,
+async def async_compute_block_years(
+    block_numbers: typing.Sequence[int] | spec.Series,
 ) -> spec.Series:
-    return await async_compute_column_block_interval(
-        block_number_series,
+    return await async_compute_block_time_bins(
+        block_numbers,
         interval_size='1y',
     )
 
 
-async def async_compute_column_block_week(
-    block_number_series,
+async def async_compute_block_weeks(
+    block_numbers: typing.Sequence[int] | spec.Series,
 ) -> spec.Series:
-    return await async_compute_column_block_interval(
-        block_number_series,
+    return await async_compute_block_time_bins(
+        block_numbers,
         interval_size='1w',
     )
 
 
-async def async_compute_column_block_day(
-    block_number_series,
+async def async_compute_block_days(
+    block_numbers: typing.Sequence[int] | spec.Series,
 ) -> spec.Series:
-    return await async_compute_column_block_interval(
-        block_number_series,
+    return await async_compute_block_time_bins(
+        block_numbers,
         interval_size='1d',
     )
 
 
-async def async_compute_column_block_hour(
-    block_number_series,
+async def async_compute_block_hours(
+    block_numbers: typing.Sequence[int] | spec.Series,
 ) -> spec.Series:
-    return await async_compute_column_block_interval(
-        block_number_series,
+    return await async_compute_block_time_bins(
+        block_numbers,
         interval_size='1h',
     )
 
 
-async def async_compute_column_block_minute(
-    block_number_series,
+async def async_compute_block_minutes(
+    block_numbers: typing.Sequence[int] | spec.Series,
 ) -> spec.Series:
-    return await async_compute_column_block_interval(
-        block_number_series,
+    return await async_compute_block_time_bins(
+        block_numbers,
         interval_size='1m',
     )
 
 
-async def async_compute_column_block_interval(
-    block_number_series,
-    interval_size: tooltime.Timelength | None = None,
+async def async_compute_block_time_bins(
+    block_numbers: typing.Sequence[int] | spec.Series,
+    interval_size: tooltime.Timelength,
     alias: str | None = None,
     context: spec.Context = None,
 ) -> spec.Series:
     block_intervals = await ctc.async_get_block_intervals(
-        start_block=block_number_series.min(),
-        end_block=block_number_series.max(),
+        start_block=min(block_numbers),
+        end_block=max(block_numbers),
         interval_size=interval_size,
         blocks_at='start',
         context={'cache': False},
     )
-    return compute_column_block_time_interval(
-        block_number_series,
+    return compute_block_time_bins(
+        block_numbers,
         block_intervals=block_intervals,
         alias=alias,
     )
 
 
-def compute_column_block_time_interval(
-    block_number_series,
+def compute_block_time_bins(
+    block_numbers: typing.Sequence[int] | spec.Series,
     block_intervals: spec.DataFrame,
     *,
     alias: str | None = None,
 ) -> spec.Series:
     import polars as pl
 
-    if block_intervals is None:
-        raise Exception('must specify block_intervals')
+    if not isinstance(block_numbers, pl.Series):
+        block_numbers = pl.Series(block_numbers)
     if alias is None:
         alias = _compute_block_intervals_alias(block_intervals)
 
     return (
-        block_number_series.cut(
-            bins=block_intervals['block'][1:],
-            labels=block_intervals['label'],
+        block_numbers.cut(
+            bins=list(block_intervals['block'][1:]),
+            labels=list(block_intervals['label']),
         )['category']
         .cast(pl.Utf8)
         .alias(alias)
     )
 
 
-def _compute_block_intervals_alias(block_intervals) -> spec.Series:
+def _compute_block_intervals_alias(
+    block_intervals: spec.DataFrame,
+) -> str:
     timestamps = block_intervals['start_timestamp']
     interval_sizes = set(timestamps[1:] - timestamps[:-1])
 
